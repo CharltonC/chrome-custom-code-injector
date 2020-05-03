6e6e2f16f6f243cb01b43b7a58dc6d920f9c8cc5
Charlton Cheng <charltoncheng86@gmail.com>
2020-05-03 16:44:09 +1000

    PAGE - Bg Script: Commented out testing/demo statements

--- src/page/bg-script/main.tsx
+++ src/page/bg-script/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Button from 'antd/es/button';
-// render(<h1>lorem sum</h1>, document.body);
 
-render(<Button type="primary">PRESS ME</Button>, document.body);
+render(<h1>lorem sum</h1>, document.body);
+
+// import Button from 'antd/es/button';
+// render(<Button type="primary">PRESS ME</Button>, document.body);
