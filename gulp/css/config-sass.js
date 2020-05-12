623224ca53633405386535eea4cb7a71285a40bf
Charlton Cheng <charltoncheng86@gmail.com>
2020-05-12 15:46:16 +1000

    BUILD - Gulp: Added new task for common css

--- gulp/css/config-sass.js
+++ gulp/css/config-sass.js
@@ -3,6 +3,11 @@ module.exports = {
         includePaths: ['node_modules'],
     },
     tasks: {
+        sharedCss: {
+            inputFiles: 'src/asset/scss/main.scss',              // NOTE: wildcard "*" is not allowed
+            rename: 'main.min.css',
+            outputPath: 'dist/build/asset/'
+        },
         popupPage: {
             inputFiles: 'src/page/option/style.scss',              // NOTE: wildcard "*" is not allowed
             rename: 'style.min.css',
