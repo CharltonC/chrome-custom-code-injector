c61e72f752dc0782140495d03932bb9775392cd3
Charlton Cheng <charltoncheng86@gmail.com>
2020-05-14 18:05:12 +1000

    BIULD - Doc: Added Storybook for UI Component Index (build+serve)

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  stories: ['../src/component/**/**/*.stories.tsx'],
+  webpackFinal: async config => {
+      // Parse Ts
+      config.module.rules.push({
+          test: /\.(ts|tsx)$/,
+          use: [{
+              loader: require.resolve('ts-loader'),
+          }],
+      });
+      config.resolve.extensions.push('.ts', '.tsx');
+      return config;
+  },
+};
