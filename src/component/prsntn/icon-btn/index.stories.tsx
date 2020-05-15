bc8b8be3e456ccbf675d9ca54a1fec579a147e62
Charlton Cheng <charltoncheng86@gmail.com>
2020-05-15 21:41:53 +1000

    CMP - Icon Btn: Added variations in doc/story

--- src/component/prsntn/icon-btn/index.stories.tsx
+++ src/component/prsntn/icon-btn/index.stories.tsx
@@ -2,46 +2,80 @@ import React from 'react';
 import { IconBtn } from '.';
 
 export default {
-  title: 'Icon Button',
-  component: IconBtn,
+    title: 'Icon Button',
+    component: IconBtn,
 };
 
 
 const icons: string[] = [
-  'setting',
-  'valid',
-  'close',
-  'power',
-  'lock-close',
-  'lock-open',
-  'radio-on',
-  'radio-off',
-  'download',
-  'add-outline',
-  'add',
-  'checkbox-off',
-  'checkbox-on',
-  'arrow-up',
-  'arrow-dn',
-  'search',
-  'arrow-rt',
-  'arrow-lt',
-  'edit',
-  'delete',
-  'save',
-  'doc'
+    'setting',
+    'valid',
+    'close',
+    'power',
+    'lock-close',
+    'lock-open',
+    'radio-on',
+    'radio-off',
+    'download',
+    'add-outline',
+    'add',
+    'checkbox-off',
+    'checkbox-on',
+    'arrow-up',
+    'arrow-dn',
+    'search',
+    'arrow-rt',
+    'arrow-lt',
+    'edit',
+    'delete',
+    'save',
+    'doc'
 ];
-const uknBtnProps = { onClick: () => { console.log('clicked'); } };
+
 const darkMode: boolean = true;
 
-export const LightIcon = () => (
-  <div style={{backgroundColor:'#5AB3AD'}}>
-    { icons.map((name) => <IconBtn icon={{name}} {...uknBtnProps} />) }
-  </div>
+export const Light = () => (
+    <div style={{ backgroundColor: '#5AB3AD' }}>
+        { icons.map((name: any) =>
+            <IconBtn
+                key={`light-${name}`}
+                icon={{ name }}
+                onClick={() => { console.log('click'); }}       // `onClick` being an "unknown" prop here
+                />
+        )}
+    </div>
 );
 
-export const DarkIcon = () => (
-  <div>
-    { icons.map((name) => <IconBtn icon={{name, darkMode}} />) }
-  </div>
+export const LightDisabled = () => (
+    <div style={{ backgroundColor: '#5AB3AD' }}>
+        { icons.map((name: any) =>
+            <IconBtn
+                key={`light-disabled-${name}`}
+                icon={{ name }}
+                disabled        // `disabled` being an "unknown" prop here
+                />
+        )}
+    </div>
+);
+
+export const Dark = () => (
+    <div>
+        { icons.map((name: any) =>
+            <IconBtn
+                key={`dark-${name}`}
+                icon={{ name, darkMode }} />
+        )}
+    </div>
+);
+
+export const DarkDisabled = () => (
+    <div>
+        { icons.map((name: any) =>
+            <IconBtn
+                key={`dark-disabled-${name}`}
+                icon={{ name, darkMode }}
+                disabled
+                />
+        )}
+    </div>
 );
