e80252657fef1922e3ee280413a0099bbfcca713
Charlton Cheng <charltoncheng86@gmail.com>
2020-05-21 17:03:52 +1000

    CMP - Icon Button: Cleaned up unit test

--- src/component/prsntn/icon-btn/index.spec.tsx
+++ src/component/prsntn/icon-btn/index.spec.tsx
@@ -1,27 +1,16 @@
-import React from "react";
-import { render } from "react-dom";
-import { act } from "react-dom/test-utils";
-
 import { TestUtil } from '../../../test-util/';
 import { IconBtn } from './';
-import * as NIconBtn from './type';
 
 describe('Component - Icon Button', () => {
     let elem: HTMLElement;
-    let icon: NIconBtn.IIcon;
-    let mockedCbFn: () => void;
-    let btnElem: Element;
+    let btnElem: HTMLElement;
+    let mockOnClick: jest.Mock;
 
     beforeEach(() => {
+        mockOnClick = jest.fn();
         elem = TestUtil.setupElem();
-
-        icon = {name: 'setting'};
-        mockedCbFn = jest.fn();
-        act(() => {
-            render(<IconBtn icon={icon} onClick={mockedCbFn} />, elem);
-        });
-
-        btnElem = elem.children[0];
+        TestUtil.renderPlain(elem, IconBtn, {icon: {name: 'setting'}, onClick: mockOnClick});
+        btnElem = elem.children[0] as HTMLElement;
     });
 
     afterEach(() => {
@@ -31,17 +20,15 @@ describe('Component - Icon Button', () => {
 
     it("should render with an icon", () => {
         const iconElems: NodeListOf<HTMLElement> = document.querySelectorAll('span.icon');
-        expect(iconElems.length).toBe(1);
-
         const iconElem: HTMLElement = iconElems[0];
+
+        expect(iconElems.length).toBe(1);
         expect(iconElem.className).toBe('icon icon--setting icon--light');
     });
 
     it('should trigger callback fn when clicked', () => {
-        act(() => {
-            btnElem.dispatchEvent(new MouseEvent('click', {bubbles: true}));
-        });
-        expect(mockedCbFn).toHaveBeenCalledTimes(1);
+        TestUtil.triggerEvt(btnElem, 'click');
+        expect(mockOnClick).toHaveBeenCalledTimes(1);
     });
 });
 
