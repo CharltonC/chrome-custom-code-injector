fe4e9d76a8c08fbf1f23863a3b9d2f311c797e0a
Charlton Cheng <charltoncheng86@gmail.com>
2020-05-26 13:35:47 +1000

    CMP - Symbol Button: Used loop to Cleanup JSX

--- src/component/prsntn/symbol-btn/index.story.tsx
+++ src/component/prsntn/symbol-btn/index.story.tsx
@@ -7,38 +7,73 @@ export default {
 };
 
 export const JsSymbol = () => {
-    // 2-way binding
+    // with state (2-way binding)
     const [ checked, setChecked ] = useState(true);
-    const onChange = (evt: Event, isChecked: boolean) => setChecked(checked);
+    const onChange = () => setChecked(!checked);
+
+    // w/o state
     const onChangeWoState = () => console.log('onChangeWoState');
+
+    const mockProps = [
+        {defaultChecked: true},
+        {defaultChecked: checked, onChange},
+        {onChange: onChangeWoState},
+        {defaultChecked: true, disabled: true},
+        {disabled: true}
+    ];
 
     return (
         <>
-            <SymbolBtn id="js-1" text="Js" />
-            <SymbolBtn id="js-2" text="Js" defaultChecked />
-            <SymbolBtn id="js-3" text="Js" defaultChecked={checked} onChange={onChange} />
-            <SymbolBtn id="js-4" text="Js" onChange={onChangeWoState} />
-            <SymbolBtn id="js-5" text="Js" defaultChecked={true} disabled />
-            <SymbolBtn id="js-5" text="Js" disabled />
+            { mockProps.map((props, idx) => (
+                    <SymbolBtn
+                        key={idx}
+                        id={`js-${idx}`}
+                        text="Js" {...props}
+                        />
+            )) }
         </>
     )
 };
 
-/*
-export const CssSymbol = () => (
-    <>
-        <SymbolBtn text="Css" defaultChecked />
-        <SymbolBtn text="Css" defaultChecked disabled />
-        <SymbolBtn text="Css" />
-        <SymbolBtn text="Css" disabled />
-    </>
-);
-export const LibSymbol = () => (
-    <>
-        <SymbolBtn text="Lib" defaultChecked />
-        <SymbolBtn text="Lib" defaultChecked disabled />
-        <SymbolBtn text="Lib" />
-        <SymbolBtn text="Lib" disabled />
-    </>
-);
- */
+
+export const CssSymbol = () => {
+    const mockProps = [
+        {defaultChecked: true},
+        {defaultChecked: true, disabled: true},
+        {},
+        {disabled: true}
+    ];
+
+    return (
+        <>
+            { mockProps.map((props, idx) => (
+                    <SymbolBtn
+                        key={idx}
+                        id={`css-${idx}`}
+                        text="Css" {...props}
+                        />
+            )) }
+        </>
+    );
+};
+
+export const LibSymbol = () => {
+    const mockProps = [
+        {defaultChecked: true},
+        {defaultChecked: true, disabled: true},
+        {},
+        {disabled: true}
+    ];
+
+    return (
+        <>
+            { mockProps.map((props, idx) => (
+                    <SymbolBtn
+                        key={idx}
+                        id={`lib-${idx}`}
+                        text="Lib" {...props}
+                        />
+            )) }
+        </>
+    );
+};
