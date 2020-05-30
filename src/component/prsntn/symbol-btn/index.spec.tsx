94d363ef64ad7acd64a1e6e0450a1f851e1036d9
Charlton Cheng <charltoncheng86@gmail.com>
2020-05-30 20:38:06 +1000

    CMP - Symbol Button: Renamed variables and Moved relevant variables into its own `describe` scope

--- src/component/prsntn/symbol-btn/index.spec.tsx
+++ src/component/prsntn/symbol-btn/index.spec.tsx
@@ -4,35 +4,35 @@ import { IProps} from './type';
 import { _TabSwitch } from '../tab-switch';
 
 describe('Component - Symbol Button', () => {
-    const mockEvt: any = { target: { checked: true}};
-    let mockProps: IProps;
-    let mockCbFn: jest.Mock;
-    let symbolBtn: _SymbolBtn;
-
-    beforeEach(() => {
-        mockProps = {id: 'js-1', text: 'Js'};
-        mockCbFn = jest.fn();
-    });
+    let mockBaseProps: IProps = {id: 'js-1', text: 'Js'};
 
     describe('Component Class', () => {
+        const mockEvt: any = { target: { checked: true}};
+        let mockCbFn: jest.Mock;
+        let symbolBtn: _SymbolBtn;
+
+        beforeEach(() => {
+            mockCbFn = jest.fn();
+        });
+
         it('constructor - should not have external state if checked state is not passed', () => {
-            symbolBtn = new _SymbolBtn({...mockProps});
+            symbolBtn = new _SymbolBtn({...mockBaseProps});
             expect(symbolBtn.hsExtState).toBe(false);
         });
 
         it('constructor - should have external state if checked state is passed', () => {
-            symbolBtn = new _SymbolBtn({...mockProps, isChecked: true});
+            symbolBtn = new _SymbolBtn({...mockBaseProps, isChecked: true});
             expect(symbolBtn.hsExtState).toBe(true);
         });
 
         it('method - should not call the passed callback `onChecked` if not provided', () => {
-            symbolBtn = new _SymbolBtn(mockProps);
+            symbolBtn = new _SymbolBtn(mockBaseProps);
             symbolBtn.onChange(mockEvt);
             expect(mockCbFn).not.toHaveBeenCalled();
         });
 
         it('method - should call the passed callback `onChecked` if provided', () => {
-            symbolBtn = new _SymbolBtn({...mockProps, onChecked: mockCbFn});
+            symbolBtn = new _SymbolBtn({...mockBaseProps, onChecked: mockCbFn});
             symbolBtn.onChange(mockEvt);
             expect(mockCbFn).toHaveBeenCalledWith(mockEvt, mockEvt.target.checked);
         });
@@ -62,24 +62,24 @@ describe('Component - Symbol Button', () => {
         });
 
         it("should pass the id, text and have default unchecked state", () => {
-            TestUtil.renderPlain($elem, SymbolBtn, mockProps);
+            TestUtil.renderPlain($elem, SymbolBtn, mockBaseProps);
             getChildElem();
 
-            expect($label.getAttribute('for')).toBe(mockProps.id);
-            expect($checkbox.id).toBe(mockProps.id);
+            expect($label.getAttribute('for')).toBe(mockBaseProps.id);
+            expect($checkbox.id).toBe(mockBaseProps.id);
             expect($checkbox.checked).toBe(false);
-            expect($span.textContent).toBe(mockProps.text);
+            expect($span.textContent).toBe(mockBaseProps.text);
         });
 
         it('should render the checked state if checked state is passed', () => {
-            TestUtil.renderPlain($elem, SymbolBtn, {...mockProps, isChecked: true});
+            TestUtil.renderPlain($elem, SymbolBtn, {...mockBaseProps, isChecked: true});
             getChildElem();
 
             expect($checkbox.checked).toBe(true);
         });
 
         it('should trigger `onChange` event handle', () => {
-            TestUtil.renderPlain($elem, SymbolBtn, mockProps);
+            TestUtil.renderPlain($elem, SymbolBtn, mockBaseProps);
             getChildElem();
 
             TestUtil.triggerEvt($label, 'click', MouseEvent);
