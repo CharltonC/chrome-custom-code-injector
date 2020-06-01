343d2b982978deee5f283a05b592d62a339a4cd1
Charlton Cheng <charltoncheng86@gmail.com>
2020-06-01 13:16:38 +1000

    CMP - Sidebar Nav: Added initial placeholder files

---
+++ src/component/prsntn-grp/sidebar-nav/index.spec.tsx
@@ -0,0 +1,35 @@
+// import React from "react";
+// import { render } from "react-dom";
+// import { act } from "react-dom/test-utils";
+
+// import { TestUtil } from '../../../test-util/';
+// // import { CmpCls } from './';
+
+// describe('Component - TODO: Component Name', () => {
+//     let elem: HTMLElement;
+//     let childElem: Element;
+
+//     beforeEach(() => {
+//         elem = TestUtil.setupElem();
+
+//         act(() => {
+//             // render(<CmpCls />, elem);
+//         });
+
+//         act(() => {
+//             // elem.dispatchEvent(new MouseEvent('click', {bubbles: true}));
+//         });
+
+//         childElem = elem.children[0];
+//     });
+
+//     afterEach(() => {
+//         TestUtil.teardown(elem);
+//         elem = null;
+//     });
+
+//     it("should render", () => {
+//         expect(true).toBe(true);
+//     });
+// });
+
