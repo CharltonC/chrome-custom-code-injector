78a8cffaedbf10a2237aa24657521787bb309816
Charlton Cheng <charltoncheng86@gmail.com>
2020-06-01 13:11:00 +1000

    BUILD - Gulp: Updated BrowserSync config to allow watch specific files only

--- gulp/util-browsersync/task.js
+++ gulp/util-browsersync/task.js
@@ -2,14 +2,26 @@ const { gulp, $path, $, util } = require('../common');
 const { defOption, watchFiles, gulpTask } = require('./config');
 
 module.exports = () => {
-    if (!$.isProd) {
-        // Watch files to rebuild
-        gulp.watch( watchFiles.html, gulp.series(gulpTask.html) );
-        gulp.watch( watchFiles.css, gulp.series(gulpTask.css) );
-        gulp.watch( watchFiles.ts, gulp.series(gulpTask.ts) );
+    const args = Object.getOwnPropertyNames($.yargs);
 
-        // Start the local server
-        $.browserSync.init(defOption);
-        gulp.watch( watchFiles.dist ).on('change', $.browserSync.reload);
-    }
+    // Prod
+    if ($.isProd) return;
+
+    // Watch Specific only (e.g. Cmd `gulp serve --css`)
+    const isWatchOnly = args.some(arg => {
+        const isMatch = arg === 'css' || arg === 'ts' || arg === 'ts';
+        if (isMatch) gulp.watch( watchFiles[arg], gulp.series(gulpTask[arg]) );
+        return isMatch;
+    });
+    if (isWatchOnly) return;
+
+    // Watch + Live Server
+    // - Watch files to rebuild
+    gulp.watch( watchFiles.html, gulp.series(gulpTask.html) );
+    gulp.watch( watchFiles.css, gulp.series(gulpTask.css) );
+    gulp.watch( watchFiles.ts, gulp.series(gulpTask.ts) );
+
+    // - Start the local server & watch otuput/build folder
+    $.browserSync.init(defOption);
+    gulp.watch( watchFiles.dist ).on('change', $.browserSync.reload);
 };
