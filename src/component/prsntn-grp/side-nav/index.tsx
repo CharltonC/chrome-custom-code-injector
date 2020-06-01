23e0d1c2a542728197d2000c9c4adb018ece6d44
Charlton Cheng <charltoncheng86@gmail.com>
2020-06-01 15:47:48 +1000

    CMP - Side Nav: Added preliminary style

--- src/component/prsntn-grp/side-nav/index.tsx
+++ src/component/prsntn-grp/side-nav/index.tsx
@@ -1,5 +1,6 @@
-import React, { Component, memo } from "react";
+import React, { Component, memo, ReactElement } from "react";
 import { IProps } from './type';
+import { inclStaticIcon } from '../../static/icon/';
 
 export class _SideNav extends Component<IProps, any> {
     constructor(props: IProps) {
@@ -7,30 +8,50 @@ export class _SideNav extends Component<IProps, any> {
     }
 
     render() {
+        const rtIconElem: ReactElement = inclStaticIcon('arrow-rt');
+        const dnIconElem: ReactElement = inclStaticIcon('arrow-dn');
+
+        // TODO: static badge
+
         return (
             <nav className="side-nav">
-                <ul className="side-nav__ls--top">
+                <ul>
                     {/* TODO: `key attr` */}
-                    <li>
-                        <ul className="side-nav__ls--nst">
+                    <li className="side-nav__ls">
+                        <p>
+                            {rtIconElem}
+                            {dnIconElem}
+                            <span>Host ID 1</span>
+                        </p>
+                        <ul>
+                            <li className="side-nav__nls">Path ID 1</li>
+                            <li className="side-nav__nls">Path ID 2</li>
+                            <li className="side-nav__nls">Path ID 3</li>
+                        </ul>
+                    </li>
+                    <li className="side-nav__ls side-nav__ls--atv">
+                        <p>
+                            {rtIconElem}
+                            {dnIconElem}
+                            <span>Host ID 2</span>
+                       </p>
+                        <ul>
                             {/* TODO: `key attr` */}
-                            <li>Path ID 1</li>
-                            <li>Path ID 2</li>
-                            <li>Path ID 3</li>
+                            <li className="side-nav__nls">Path ID 1</li>
+                            <li className="side-nav__nls">Path ID 2</li>
+                            <li className="side-nav__nls">Path ID 3</li>
                         </ul>
                     </li>
-                    <li>
+                    <li className="side-nav__ls">
+                        <p>
+                            {rtIconElem}
+                            {dnIconElem}
+                            <span>Host ID 3</span>
+                        </p>
                         <ul>
-                            <li>Path ID 1</li>
-                            <li>Path ID 2</li>
-                            <li>Path ID 3</li>
-                        </ul>
-                    </li>
-                    <li>
-                        <ul>
-                            <li>Path ID 1</li>
-                            <li>Path ID 2</li>
-                            <li>Path ID 3</li>
+                            <li className="side-nav__nls">Path ID 1</li>
+                            <li className="side-nav__nls">Path ID 2</li>
+                            <li className="side-nav__nls">Path ID 3</li>
                         </ul>
                     </li>
                 </ul>
--- src/component/prsntn-grp/side-nav/type.ts
+++ src/component/prsntn-grp/side-nav/type.ts
@@ -1,3 +1,11 @@
-export interface IProps extends React.HTMLAttributes<HTMLElement> {
+export interface IListItem {
+    name: string;
+}
 
+export interface IList extends IListItem {
+    nedstList?: IListItem[];
+}
+
+export interface IProps extends React.HTMLAttributes<HTMLElement> {
+    // list: IList[];
 }
