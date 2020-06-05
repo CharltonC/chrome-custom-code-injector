6ccff4fdafb37e5cc32d2764abcdd2b0c65d67f5
Charlton Cheng <charltoncheng86@gmail.com>
2020-06-05 16:40:55 +1000

    BUILD - Doc: Added debug log logic and comment for Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,18 @@
 module.exports = {
-  stories: ['../src/component/**/**/*.story.tsx'],
-  webpackFinal: async config => {
-      // Parse Ts
-      config.module.rules.push({
-          test: /\.(ts|tsx)$/,
-          use: [{
-              loader: require.resolve('ts-loader'),
-          }],
-      });
-      config.resolve.extensions.push('.ts', '.tsx');
-      return config;
-  },
+    stories: ['../src/component/**/**/*.story.tsx'],
+    webpackFinal: async config => {
+        // Parse Ts
+        config.module.rules.push({
+            test: /\.(ts|tsx)$/,
+            use: [{
+                loader: require.resolve('ts-loader'),
+            }],
+        });
+        config.resolve.extensions.push('.ts', '.tsx');
+
+        // For Debugging, use the following setting alone + CMD `npm run ui --debug-webpack`:
+        // console.dir(config, { depth: null })
+
+        return config;
+    }
 };
