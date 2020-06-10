e4a1ff8756b234acb3b0641c92b9007920400e9f
Charlton Cheng <charltoncheng86@gmail.com>
2020-06-10 14:36:53 +1000

    BUILD - TS: Babel_Allowed compile for specific ES6 import/export from "node_modules" path

--- babel.config.js
+++ babel.config.js
@@ -30,5 +30,10 @@ module.exports = {
          */
         "@babel/preset-env", "@babel/preset-react",
         '@babel/preset-typescript'
+    ],
+
+    global: true,
+    ignore: [
+        './node_modules/@fluentui/react/lib'
     ]
 };
