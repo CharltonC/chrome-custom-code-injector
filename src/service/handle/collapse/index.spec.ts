c5eccf2a70903bd222243d6771ee1e37038a7928
Charlton Cheng <charltoncheng86@gmail.com>
2020-06-20 14:56:58 +1000

    SERVICE - Collapse Handle: Completed test for isNestedRowOpen Method

--- src/service/handle/collapse/index.spec.ts
+++ src/service/handle/collapse/index.spec.ts
@@ -1,3 +1,4 @@
+import { TClpsShowTarget } from './type';
 import { ClpsHandle } from './';
 
 describe('Service - Collapse Handle', () => {
@@ -8,21 +9,28 @@ describe('Service - Collapse Handle', () => {
         handle = new ClpsHandle();
     });
 
-
     describe('Method - isNestedRowOpen: Check if a row should open/collapse its nested rows', () => {
+        const { isNestedRowOpen } = ClpsHandle.prototype;
+
         describe('when show target context is an array of contexts', () => {
-            const mockShowTargetCtx: string[] = [ 'a', 'a/b' ];
+            const mockShowTargetCtx: TClpsShowTarget = [ 'a', 'a/b' ];
 
             it('should return false if row context is found in the show target context', () => {
-                const mockRowCtx: string = 'a/b/c';
-                const isOpen: boolean = handle.isNestedRowOpen(mockRowCtx, mockShowTargetCtx);
-                expect(isOpen).toBe(false);
+                expect(isNestedRowOpen('a/b/c', mockShowTargetCtx)).toBe(false);
             });
 
             it('should return true if row context is found in the show target context', () => {
-                const mockRowCtx: string = 'a/b';
-                const isOpen: boolean = handle.isNestedRowOpen(mockRowCtx, mockShowTargetCtx);
-                expect(isOpen).toBe(true);
+                expect(isNestedRowOpen('a/b', mockShowTargetCtx)).toBe(true);
+            });
+        });
+
+        describe('when show target context is `ALL` or `NONE`', () => {
+            it('should return true if show target context is show all', () => {
+                expect(isNestedRowOpen('', 'ALL')).toBe(true);
+            });
+
+            it('should return false if show target context is show none', () => {
+                expect(isNestedRowOpen('', 'NONE')).toBe(false);
             });
         });
 
