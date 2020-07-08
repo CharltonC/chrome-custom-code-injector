60fc8f7809670f2c2868441c4c4da6bf3876a9bb
Charlton Cheng <charltoncheng86@gmail.com>
2020-07-08 12:57:10 +1000

    SERVICE - Pagination Handle: Formatted order, added comment, added new typing for pagination option

--- src/service/handle/paginate/type.ts
+++ src/service/handle/paginate/type.ts
@@ -1,21 +1,17 @@
-export interface IPageRange {
-    curr: number;
-    last: number;
-}
-
-export interface IPageSlice {
-    startIdx: number;
-    endIdx: number;
-}
-
-export interface IPageNavQuery {
-    type: string;
-    target?: number;
+//// Pagination State/Status
+export interface IPgnStatus extends IPageCtx, IPageSlice, IRelPage, IRecordCtx {
+    perPage: number;
+    totalPage: number;
 }
 
 export interface IPageCtx {
     curr: number;
     pageNo: number;
+}
+
+export interface IPageSlice {
+    startIdx: number;
+    endIdx: number;
 }
 
 export type IRelPage = {
@@ -25,19 +21,36 @@ export type IRelPage = {
     last: number;
 }
 
-type TRelPageCtx = {
-    [K in keyof IRelPage]: boolean;
-}
-
-export interface IRelPageCtx extends TRelPageCtx {}
-
 export interface IRecordCtx {
     totalRecord: number;
     startRecord: number;
     endRecord: number;
 }
 
-export interface IPgnStatus extends IPageCtx, IPageSlice, IRelPage, IRecordCtx {
-    perPage: number;
-    totalPage: number;
+
+//// Pagination Option (full)
+export interface IOption {
+    page: number;
+    increment: number;
+    incrementIdx: number;
+}
+
+
+//// Pagination Request/Query
+// Current Page Index Range
+export interface IPageRange {
+    curr: number;
+    last: number;
+}
+
+// Navigation Target Request
+export interface IPageNavQuery {
+    type: string;
+    target?: number;
+}
+
+// Whether relevant pages are navigatable
+export interface IRelPageCtx extends TRelPageCtx {}
+type TRelPageCtx = {
+    [K in keyof IRelPage]: boolean;
 }
