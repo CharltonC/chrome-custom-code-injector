a4e853d2a5f0489c8e343fed21d02132f4fa0de4
Charlton Cheng <charltoncheng86@gmail.com>
2020-07-11 17:53:18 +1000

    CMP - Pagination: Changed props to fit to the interface from Pagination Handle

--- src/component/prsntn/pagination/index.tsx
+++ src/component/prsntn/pagination/index.tsx
@@ -1,115 +1,127 @@
-import React, { ReactElement, Component, memo } from "react";
+import React, { ReactElement, memo } from "react";
 
 import { Dropdown } from '../dropdown';
 import { inclStaticIcon } from '../../static/icon';
 
-import { IProps, TSelectEvt, TEvtHandler } from './type';
+import { IProps } from './type';
 
-const ltArrowElem: ReactElement = inclStaticIcon('arrow-lt');
-const rtArrowElem: ReactElement = inclStaticIcon('arrow-rt');
+export const _Pagination = (props: IProps) => {
+    const CLS_PREFIX: string = 'kz-paginate';
+    const ltArrowElem: ReactElement = inclStaticIcon('arrow-lt');
+    const rtArrowElem: ReactElement = inclStaticIcon('arrow-rt');
 
-export class _Pagination extends Component<IProps> {
-    readonly CLS_PREFIX: string = 'kz-paginate';
+    const {
+        startRecord,
+        endRecord,
+        totalRecord,
+        pageNo,
+        totalPage,
+        first: firstPage,
+        firstBtnAttr: first,
+        prevBtnAttr: prev,
+        nextBtnAttr: next,
+        lastBtnAttr: last,
+        ltSpreadBtnsAttr: ltSpread,
+        rtSpreadBtnsAttr: rtSpread,
+        perPageSelectAttr: perPage,
+        pageSelectAttr: page
+    } = props;
 
-    render() {
-        const { CLS_PREFIX } = this;
-        const {
-            // Text
-            startRecord, endRecord, totalRecord,
-
-            // Buttons
-            first, prev, next, last,
-
-            // Page Select
-            pageList, pageSelectIdx,
-
-            // Per Page Select
-            increment, incrementIdx,
-
-            // Callback for Buttons & Selects
-            onPgnChange
-        } = this.props;
-
-        const pageSelectProps = this.createPageSelectProps(pageList, pageSelectIdx, onPgnChange);
-        const perPageSelectProps = this.createPerPageSelectProps(increment, incrementIdx, onPgnChange);
-        const firstBtnProps = this.createBtnProps('first', first, onPgnChange);
-        const prevBtnProps = this.createBtnProps('prev', prev, onPgnChange);
-        const nextBtnProps = this.createBtnProps('next', next, onPgnChange);
-        const lastBtnProps = this.createBtnProps('last', last, onPgnChange);
-
-        return (
-            <div className={CLS_PREFIX}>
-                <p>Showing {startRecord} - {endRecord} of {totalRecord}</p>
-                <Dropdown {...pageSelectProps} />
-                <Dropdown {...perPageSelectProps} />
-                <button {...firstBtnProps} />
-                <button {...prevBtnProps} />
-                <button {...nextBtnProps} />
-                <button {...lastBtnProps} />
-            </div>
-        );
-    }
-
-    createBtnProps(name: string, pageIdx: number, onPgnChange: TEvtHandler) {
-        const CLS_SUFFIX: string = `btn-${name}`;
-
-        let children: ReactElement | ReactElement[];
-        switch(name) {
-            case 'first':
-                children = [ltArrowElem, ltArrowElem];
-                break;
-            case 'prev':
-                children = ltArrowElem;
-                break;
-            case 'next':
-                children = rtArrowElem;
-                break;
-            case 'last':
-                children = [rtArrowElem, rtArrowElem];
-                break;
-        }
-
-        return {
-            key: CLS_SUFFIX,
-            type: 'button',
-            className: `${this.CLS_PREFIX}__${CLS_SUFFIX}`,
-            disabled: !Number.isInteger(pageIdx),
-            children,
-            onClick: () => onPgnChange({page: pageIdx})
-        } as any;
-    }
-
-    createPageSelectProps(pageList: number[], pageSelectIdx: number, onPgnChange: TEvtHandler) {
-        const CLS_SUFFIX: string = 'select-page';
-
-        return {
-            key: CLS_SUFFIX,
-            className: `${this.CLS_PREFIX}__${CLS_SUFFIX}`,
-            border: true,
-            disabled: pageList.length <= 1,
-            selectIdx: pageSelectIdx,
-            list: pageList,
-            listTxtTransform: (pageIdx: number) => `Page ${pageIdx+1}`,
-            onSelect: ({target}: TSelectEvt) => onPgnChange({page: pageList[parseInt(target.value, 10)]}),
-        };
-    }
-
-    createPerPageSelectProps(increment: number[], incrementIdx: number, onPgnChange: TEvtHandler) {
-        const CLS_SUFFIX: string = 'select-perpage';
-
-        return {
-            key: CLS_SUFFIX,
-            className: `${this.CLS_PREFIX}__${CLS_SUFFIX}`,
-            border: true,
-            value: incrementIdx,
-            list: increment,
-            listTxtTransform: (perPage: number) => `${perPage} Per Page`,
-            onSelect: ({target}: TSelectEvt) => onPgnChange({
-                incrementIdx: parseInt(target.value, 10),
-                page: 0
-            })
-        };
-    }
-}
+    return (
+        <div className={CLS_PREFIX}>
+            <p>Showing {startRecord} - {endRecord} of {totalRecord}</p>
+            <Dropdown
+                border={true}
+                list={perPage.options}
+                listTxtTransform={(p) => `${p} Per Page`}
+                selectIdx={perPage.selectedOptionIdx}
+                onSelect={perPage.onEvt}
+                />
+            <Dropdown
+                border={true}
+                disabled={page.isDisabled}
+                list={page.options}
+                listTxtTransform={(p) => Number.isInteger(p) ? `Page ${p}` : `${p}`}
+                selectIdx={page.selectedOptionIdx}
+                onSelect={page.onEvt}
+                />
+            <button
+                className={`${CLS_PREFIX}__btn-first`}
+                type="button"
+                disabled={first.isDisabled}
+                onClick={first.onEvt}
+                >
+                {ltArrowElem}{ltArrowElem}
+            </button>
+            <button
+                className={`${CLS_PREFIX}__btn-prev`}
+                type="button"
+                disabled={prev.isDisabled}
+                onClick={prev.onEvt}
+                >
+                {ltArrowElem}
+            </button>
+            <button
+                className={`${CLS_PREFIX}__btn-next`}
+                type="button"
+                disabled={next.isDisabled}
+                onClick={next.onEvt}
+                >
+                {rtArrowElem}
+            </button>
+            <button
+                className={`${CLS_PREFIX}__btn-last`}
+                type="button"
+                disabled={last.isDisabled}
+                onClick={last.onEvt}
+                >
+                {rtArrowElem}{rtArrowElem}
+            </button>
+            <section>
+                {!first.isDisabled &&
+                <button
+                    className={`${CLS_PREFIX}__btn-first`}
+                    type="button"
+                    onClick={first.onEvt}
+                    >
+                    1
+                </button>}
+                {ltSpread?.map(({title, onEvt}, idx) => (
+                    <button
+                        key={`lt-spread-${idx}`}
+                        type="button"
+                        onClick={onEvt}
+                        >
+                        {typeof title === 'number' ? title : '...'}
+                    </button>
+                ))}
+                <button
+                    type="button"
+                    disabled={true}
+                    >
+                    {pageNo}
+                    </button>
+                {rtSpread?.map(({title, onEvt}, idx) => (
+                    <button
+                    key={`rt-spread-${idx}`}
+                        type="button"
+                        onClick={onEvt}
+                        >
+                        {typeof title === 'number' ? title : '...'}
+                    </button>
+                ))}
+                {!last.isDisabled &&
+                <button
+                    className={`${CLS_PREFIX}__btn-last`}
+                    type="button"
+                    onClick={last.onEvt}
+                    >
+                    {totalPage}
+                    </button>
+                }
+            </section>
+        </div>
+    );
+};
 
 export const Pagination = memo(_Pagination);
--- src/component/prsntn/pagination/type.ts
+++ src/component/prsntn/pagination/type.ts
@@ -1,13 +1,4 @@
 import * as pgnHandleType from '../../../service/handle/paginate/type';
 
-export interface IProps extends pgnHandleType.IRecordCtx, pgnHandleType.IRelPage {
-    pageList: number[];
-    pageSelectIdx: number;
-    increment: number[];
-    incrementIdx: number;
-    onPgnChange: (...args: any[]) => any;
+export interface IProps extends pgnHandleType.ICmpAttr, pgnHandleType.IState {
 }
-
-export type TSelectEvt = React.ChangeEvent<HTMLSelectElement>;
-
-export type TEvtHandler = (...args: any[]) => void;
