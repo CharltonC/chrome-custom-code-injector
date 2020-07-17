32c7c21d3f68da5e2d5c5166b54ffd6c04a1e007
Charlton Cheng <charltoncheng86@gmail.com>
2020-07-17 21:22:23 +1000

    CMP - Data Grid: Cleaned up types

--- src/component/composition/data-grid/type.ts
+++ src/component/composition/data-grid/type.ts
@@ -1,5 +1,3 @@
-import { ReactElement } from "react";
-
 import * as sortHandleType from '../../../service/handle/sort/type';
 import * as rowHandleType from '../../../service/handle/row/type';
 import * as pgnHandleType from '../../../service/handle/pagination/type';
@@ -14,18 +12,18 @@ export interface IProps extends React.HTMLAttributes<HTMLElement> {
     type?: TGridType;
     header?: thHandleType.IOption[];
     expand?: IExpandOption;
-    sort?: TSortOption;
+    sort?: Partial<sortHandleType.IOption>;
     paginate?: Partial<pgnHandleType.IOption>;
     callback?: ICallbackOption
 }
 
-export type TSortOption = Partial<sortHandleType.IOption>;
-
+// TODO: Move to Expand Wrapper Cmp Type
 export interface IExpandOption {
     showInitial?: rowHandleType.TVisibleNestablePath;
     showOnePerLvl?: boolean;
 }
 
+// TODO: Renamed to IRowOption
 export interface IRow extends Array<any> {
     0: string | TRowCmpCls;
     1?: TRowCmpCls;
@@ -38,28 +36,19 @@ export interface ICallbackOption {
 }
 
 export type TRowCmpCls = React.FC<any> | React.ComponentClass<any>;
-
 export type TRowKeyPipeFn = (ctx: rowHandleType.IItemCtx) => string;
-
 export type TGridType = 'table' | 'list';
 
 
 //// State
 export interface IState {
     rowOption: rowHandleType.IRawRowConfig[];
-    sortOption: TSortOption;
-    // TODO: fix
+    sortOption: sortHandleType.IOption;
+    // TODO: fix sortState type
     sortState: any;
     pgnOption: pgnHandleType.IOption;
     pgnState: pgnHandleType.IState;
     thState: thHandleType.IThCtx[][]
-}
-
-export type TExpandState = Record<string, boolean>;
-
-export interface INestedRowProps extends React.HTMLAttributes<HTMLElement> {
-    item: {[k: string]: any};
-    nestedRow?: ReactElement;
 }
 
 export type TShallResetState = {
@@ -67,16 +56,8 @@ export type TShallResetState = {
 }
 
 
-//// Expand/Collapse
-export interface IClpsProps {
-    isNestedOpen?: boolean;
-    onCollapseChanged?: TFn;
-}
-
 //// Generic
 export type TFn = (...args: any[]) => any;
-export type TBtnProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
-export type TSelectEvt = React.ChangeEvent<HTMLSelectElement>;
 
 
 //// Reexport
