e049aa93b2acd78dfc273f63daf47f17e115d829
Charlton Cheng <charltoncheng86@gmail.com>
2020-07-21 19:49:17 +1000

    SERVICE - Utility Handle: Added test

---
+++ src/service/handle/util/index.spec.ts
@@ -0,0 +1,14 @@
+import { UtilHandle } from './';
+
+describe('Utility Handle', () => {
+    describe('method - cssCls: Get the Class Class based on a base class and a suffix', () => {
+        const { cssCls } = UtilHandle.prototype;
+
+        it('should return the css class', () => {
+            const MOCK_BASE_CLS: string = 'lorem';
+            const MOCK_SUFFIX_CLS: string = 'sum';
+
+            expect(cssCls(MOCK_BASE_CLS, MOCK_SUFFIX_CLS)).toBe(`${MOCK_BASE_CLS} ${MOCK_BASE_CLS}--${MOCK_SUFFIX_CLS}`);
+        });
+    });
+});
