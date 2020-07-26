decdbd7e3c78ec11d9913a64211266f2f426b044
Charlton Cheng <charltoncheng86@gmail.com>
2020-07-26 14:31:29 +1000

    ASSET - Ts: Memo Component -- Added check logic for local state changes and added tests

--- src/asset/ts/memo-component/index.spec.tsx
+++ src/asset/ts/memo-component/index.spec.tsx
@@ -2,32 +2,49 @@ import { MemoComponent } from './';
 
 describe('Memo Component', () => {
     let memoCmp: MemoComponent;
-    const mockNonEmptyProps = { lorem: 1 };
-    const setPropsVal = (val) => {
-        (memoCmp as any).props = val;
-    };
 
     beforeEach(() => {
         memoCmp = new MemoComponent({});
     });
 
-    it('should return false when there are no new props', () => {
-        setPropsVal(mockNonEmptyProps);
-        expect(memoCmp.shouldComponentUpdate({})).toBeFalsy();
+    describe('Check State Changes', () => {
+        const mockEmptyProps = {};
+        const mockState = { lorem: 1 };
+        const setStateVal = (val) => {
+            (memoCmp as any).state = val;
+        };
+
+        it('should return true when new states are not empty and are different to current state values', () => {
+            setStateVal(mockState);
+            expect(memoCmp.shouldComponentUpdate(mockEmptyProps, { lorem: 2})).toBeTruthy();
+        });
     });
 
-    it('should return false when there are no current props', () => {
-        setPropsVal({});
-        expect(memoCmp.shouldComponentUpdate({})).toBeFalsy();
-    });
+    describe('Check Props Changes', () => {
+        const mockProps = { lorem: 1 };
+        const mockEmptyState = {};
+        const setPropsVal = (val) => {
+            (memoCmp as any).props = val;
+        };
 
-    it('should return false when all new prop values are the same as current prop values', () => {
-        setPropsVal(mockNonEmptyProps);
-        expect(memoCmp.shouldComponentUpdate(mockNonEmptyProps)).toBeFalsy();
-    });
+        it('should return false when there are no new props', () => {
+            setPropsVal(mockProps);
+            expect(memoCmp.shouldComponentUpdate({}, mockEmptyState)).toBeFalsy();
+        });
 
-    it('should return true when one of new prop values is different to the current prop values ', () => {
-        setPropsVal(mockNonEmptyProps);
-        expect(memoCmp.shouldComponentUpdate({ lorem: 2 })).toBeTruthy();
+        it('should return false when there are no current props', () => {
+            setPropsVal({});
+            expect(memoCmp.shouldComponentUpdate({}, mockEmptyState)).toBeFalsy();
+        });
+
+        it('should return false when all new prop values are the same as current prop values', () => {
+            setPropsVal(mockProps);
+            expect(memoCmp.shouldComponentUpdate(mockProps, mockEmptyState)).toBeFalsy();
+        });
+
+        it('should return true when one of new prop values is different to the current prop values ', () => {
+            setPropsVal(mockProps);
+            expect(memoCmp.shouldComponentUpdate({ lorem: 2 }, mockEmptyState)).toBeTruthy();
+        });
     });
 });
--- src/asset/ts/memo-component/index.tsx
+++ src/asset/ts/memo-component/index.tsx
@@ -4,17 +4,26 @@ import { Component } from 'react';
  * Used for Component Class Inheritance only for React Component
  */
 export class MemoComponent<P = any, S = any> extends Component<P, S> {
-    shouldComponentUpdate(modProps: Record<string, any>) {
-        const modPropsEntries: [string, any][] = Object.entries(modProps);
-        const currPropsEntries: [string, any][] = Object.entries(this.props);
+    shouldComponentUpdate(modProps: Record<string, any>, modState: Record<string, any>) {
+        // Check State 1st (since internal state changes should be prioritized)
+        const modStateItems: [string, any][] = Object.entries(modState);
+        const isDiffState: boolean = modStateItems.length &&
+            modStateItems.some(([key, val]: [string, any]) => {
+                return val !== this.state[key];
+            });
+        // - Update if any state value changes, else Proceed to checking Props
+        if (isDiffState) return true;
 
-        // Dont update if no props
-        if (!modPropsEntries.length || !currPropsEntries.length) return false;
+        // Check Props
+        const modPropsItems: [string, any][] = Object.entries(modProps);
+        const currPropsItems: [string, any][] = Object.entries(this.props);
 
-        // Check by using strict equal:
-        // - if any prop val not equal, then update
-        return modPropsEntries.some(([propKey, propVal]: [string, any]) => {
-            return propVal !== this.props[propKey];
+        // - Dont update if no props
+        if (!modPropsItems.length || !currPropsItems.length) return false;
+
+        // - Check by using strict equal: if any prop val not equal, then update
+        return modPropsItems.some(([key, val]: [string, any]) => {
+            return val !== this.props[key];
         });
     }
 }
