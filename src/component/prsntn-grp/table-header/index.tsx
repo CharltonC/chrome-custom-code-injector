70b0e15d23e3db1fb8a2816dae7ed09088df0590
Charlton Cheng <charltoncheng86@gmail.com>
2020-07-28 15:59:36 +1000

    CMP - Table Header: Renamed class name

--- src/component/prsntn-grp/table-header/index.tsx
+++ src/component/prsntn-grp/table-header/index.tsx
@@ -4,7 +4,7 @@ import { IProps, thHandleType } from './type';
 
 export const _TableHeader: FC<IProps> = ({ thRowsContext, sortBtnProps } : IProps) => {
     return (
-        <thead className="kz-tableheader">{ thRowsContext.map((thCtxs: thHandleType.IThCtx[], trIdx: number) => (
+        <thead className="kz-datagrid__head">{ thRowsContext.map((thCtxs: thHandleType.IThCtx[], trIdx: number) => (
             <tr key={trIdx}>{ thCtxs.map( ({ title, sortKey, ...thProps }: thHandleType.IThCtx, thIdx: number) => (
                 <th key={thIdx} {...thProps}>
                     <span>{title}</span>{ sortKey && sortBtnProps &&
