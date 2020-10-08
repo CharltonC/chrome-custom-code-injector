1dced0a761b22740d529eba2280d4f1d42f6ea82
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-08 12:55:53 +1100

    CMP - Modal: Added cancel/confirm buttons in examples

--- src/component/widget/modal/index.story.tsx
+++ src/component/widget/modal/index.story.tsx
@@ -16,20 +16,33 @@ export const ForClassComponent = () => {
     class Demo extends Component {
         modal2Ctrl: any;
 
+        openModalA() {
+            (this.refs.demoModalA as any).onOpen();
+        }
+
+        closeModalA() {
+            (this.refs.demoModalA as any).onClose();
+        }
+
         render() {
             return (
                 <div style={defStyle}>
                     {/* Example 1 - using `this.refs` */}
-                    <button type="button" onClick={e => (this.refs.demoModalA as any).onOpen(e)}>Open modal A</button>
+                    <button type="button" onClick={this.openModalA.bind(this)}>Open modal A</button>
                     <Modal headerText="title" ref="demoModalA">
                         <h1>Modal A content</h1>
+                        <TextBtn text="cancel" outline onClick={this.closeModalA.bind(this)} />
+                        <TextBtn text="confirm" onClick={this.closeModalA.bind(this)} />
                     </Modal>
                     <br/>
                     {/* Example 2 - using class property `modal2Ctrl` */}
                     <button type="button" onClick={() => this.modal2Ctrl.onOpen()}>Open modal B</button>
                     <Modal headerText="title" ref={ ctrl => this.modal2Ctrl = ctrl }>
                         <h1>Modal B content</h1>
+                        <TextBtn text="cancel" outline onClick={() => this.modal2Ctrl.onClose()} />
+                        <TextBtn text="confirm" onClick={() => this.modal2Ctrl.onClose()} />
                     </Modal>
+
                 </div>
             );
         }
@@ -42,19 +55,22 @@ export const ForFunctionalComponent = () => {
     let demoModalCtrlA;
     let demoModalCtrlB;
     const openModalB = () => demoModalCtrlB?.onOpen();
+    const closeModalB = () => demoModalCtrlB?.onClose();
 
     return (
         <div style={defStyle}>
-            <button type="button" onClick={e => demoModalCtrlA.onOpen(e)}>Open modal A</button>
+            <button type="button" onClick={() => demoModalCtrlA.onOpen()}>Open modal A</button>
             <Modal headerText="title" ref={ modalCtrl => demoModalCtrlA = modalCtrl }>
                 <h1>Modal A content</h1>
+                <TextBtn text="cancel" outline onClick={() => demoModalCtrlA.onClose()} />
+                <TextBtn text="confirm" onClick={() => demoModalCtrlA.onClose()} />
             </Modal>
             <br/>
             <button type="button" onClick={openModalB}>Open modal B</button>
             <Modal headerText="title" ref={ modalCtrl => demoModalCtrlB = modalCtrl }>
                 <h1>Modal B content</h1>
-                <TextBtn text="cancel" outline />
-                <TextBtn text="confirm" />
+                <TextBtn text="cancel" outline onClick={closeModalB} />
+                <TextBtn text="confirm" onClick={closeModalB} />
             </Modal>
         </div>
     );
