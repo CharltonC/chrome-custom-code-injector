687d3e19a578a8659ff98bbdd9ea6c41d9e7a4d9
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-09 20:16:29 +1100

    CMP - App: Corrected class name in html

--- src/component/app/popup/index.tsx
+++ src/component/app/popup/index.tsx
@@ -5,7 +5,7 @@ import { IProps } from './type';
 
 export const PopupApp: React.FC<IProps> = memo((props: IProps) => {
     return (
-        <div className="view view--popup">
+        <div className="app-popup">
             <nav>
                 <IconBtn icon="doc" theme="white" />
                 <IconBtn icon="option" theme="white" />
