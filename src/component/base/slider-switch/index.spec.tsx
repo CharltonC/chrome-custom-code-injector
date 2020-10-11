e0280911ef5fe4b6c47ac1a3f7af94b8fa4b0964
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-11 12:44:11 +1100

    CMP - Slider Switch: Added test for label render

--- src/component/base/slider-switch/index.spec.tsx
+++ src/component/base/slider-switch/index.spec.tsx
@@ -14,11 +14,17 @@ describe('Component - Slider Switch', () => {
         const MOCK_ID = 'id';
         const MOCK_LABEL = 'label';
 
-        it('should render', () => {
-            shallow.render(<SliderSwitch id={MOCK_ID} label={MOCK_LABEL} />);
+        it('should render without label', () => {
+            shallow.render(<SliderSwitch id={MOCK_ID} />);
             const [ childOne, childTwo ] = shallow.getRenderOutput().props.children;
             expect(childOne.props.id).toBe(MOCK_ID);
-            expect(childTwo.type).toBe('span');
+            expect(childTwo).toBeFalsy();
+        });
+
+        it('should render with label', () => {
+            shallow.render(<SliderSwitch id={MOCK_ID} label={MOCK_LABEL} />);
+            const { type } = shallow.getRenderOutput().props.children[1];
+            expect(type).toBe('span');
         });
     });
 });
