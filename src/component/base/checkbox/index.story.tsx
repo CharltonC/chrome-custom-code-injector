57987c9079b88e28c6a2c13b26edaa91ad0f40c8
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-11 13:41:03 +1100

    CMP - Checkbox: Added checkbox with tests

---
+++ src/component/base/checkbox/index.story.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Checkbox } from '.';
+
+export default {
+    title: 'Checkbox',
+    component: Checkbox,
+};
+
+const defStyle = {};
+
+export const Default = () => {
+    return (
+        <div style={defStyle} >
+            <Checkbox id="lorem" label="checkbox" />
+        </div>
+    )
+};
+
+export const Disabled = () => {
+    return (
+        <div style={defStyle} >
+            <Checkbox id="lorem" label="checkbox" disabled />
+        </div>
+    )
+};
+
+export const ExternalState = () => {
+    const [ checked, setChecked ] = useState(true);
+
+    return (
+        <div style={defStyle} >
+            <Checkbox
+                id="lorem"
+                label="checkbox"
+                checked={checked}
+                onClick={() => setChecked(!checked)}
+                />
+        </div>
+    )
+};
---
+++ src/component/base/checkbox/index.tsx
@@ -0,0 +1,19 @@
+import React, { memo } from 'react';
+import { IProps } from './type';
+
+export const Checkbox: React.FC<IProps> = memo((props: IProps) => {
+    const { id, label, ...inputProps } = props;
+
+    return (
+        <label className="checkbox" htmlFor={id}>
+            <input
+                type="checkbox"
+                id={id}
+                name={id}
+                {...inputProps}
+                />
+                { label &&
+                <span>{label}</span>}
+        </label>
+    );
+});
