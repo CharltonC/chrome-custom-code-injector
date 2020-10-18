e561625a4b6e10f21a75f4f1c744ae0f5ec05bfa
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-18 20:55:01 +1100

    CMP - Tab Switch: Moved non-builtin methods after builtin methods

--- src/component/base/tab-switch/index.tsx
+++ src/component/base/tab-switch/index.tsx
@@ -12,28 +12,6 @@ export class TabSwitch extends MemoComponent<IProps, IState> {
         this.onCheckboxChanged = this.onCheckboxChanged.bind(this);
     }
 
-    getInitialState(list: ITabItem[], activeIdx: number): IState {
-        const hsList: boolean = typeof list !== 'undefined' && !!list.length;
-        const hsAtvIdx: boolean = typeof activeIdx !== 'undefined' && !!list[activeIdx];
-        return {
-            hsList,
-            hsAtvIdx,
-            activeTab: hsList ? (hsAtvIdx ? list[activeIdx] : list[0]) : null
-        };
-    }
-
-    onRdoChecked(evt: React.ChangeEvent<HTMLInputElement>, activeTab: ITabItem, idx: number): void {
-        const isActive: boolean = this.state.activeTab === activeTab;
-        if (!isActive) this.setState({activeTab});
-        this.props.onTabActive?.({evt, activeTab, idx, isActive});
-    }
-
-    onCheckboxChanged(evt: React.ChangeEvent<HTMLInputElement>, tab: ITabItem, idx: number): void {
-        const isActive: boolean = this.state.activeTab === tab;
-        const isEnable: boolean = !tab.isEnable;
-        this.props.onTabEnable?.({evt, tab, idx, isEnable, isActive});
-    }
-
     render() {
         const { id, list, activeIdx } = this.props;
         const { hsList, hsAtvIdx, activeTab } = this.state;
@@ -83,4 +61,27 @@ export class TabSwitch extends MemoComponent<IProps, IState> {
             </ul> :
             null;
     }
+
+    getInitialState(list: ITabItem[], activeIdx: number): IState {
+        const hsList: boolean = typeof list !== 'undefined' && !!list.length;
+        const hsAtvIdx: boolean = typeof activeIdx !== 'undefined' && !!list[activeIdx];
+        return {
+            hsList,
+            hsAtvIdx,
+            activeTab: hsList ? (hsAtvIdx ? list[activeIdx] : list[0]) : null
+        };
+    }
+
+    onRdoChecked(evt: React.ChangeEvent<HTMLInputElement>, activeTab: ITabItem, idx: number): void {
+        const isActive: boolean = this.state.activeTab === activeTab;
+        if (!isActive) this.setState({activeTab});
+        this.props.onTabActive?.({evt, activeTab, idx, isActive});
+    }
+
+    onCheckboxChanged(evt: React.ChangeEvent<HTMLInputElement>, tab: ITabItem, idx: number): void {
+        const isActive: boolean = this.state.activeTab === tab;
+        const isEnable: boolean = !tab.isEnable;
+        this.props.onTabEnable?.({evt, tab, idx, isEnable, isActive});
+    }
+
 }
