9370f745312839b45f1da59cdc58f8f0dd6c3a37
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-24 13:47:27 +1100

    SERVICE - Modal ID constant: Added constant

---
+++ src/service/constant/modal-id/index.ts
@@ -0,0 +1,12 @@
+class ModalId {
+    SETTING = 'modal-setting';
+    SETTING_IMPORT = 'modal-setting-import';
+    SETTING_EXPORT = 'modal-setting-export';
+    DELETE = 'modal-delete-confirm';
+    HOST_ADD = 'modal-host-add';
+    PATH_ADD = 'modal-path-add';
+    LIB_ADD = 'modal-lib-add';
+    LIB_EDIT = 'modal-lib-edit';
+};
+
+export const modalId = new ModalId();
