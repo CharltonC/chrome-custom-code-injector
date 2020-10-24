3d91cb60c24b30ecb043e28a62e667123998f79a
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-24 15:49:59 +1100

    SERVICE - Local State Model: Added model (moved from `local-state` state)

---
+++ src/service/model/local-state/index.ts
@@ -0,0 +1,23 @@
+import { AView } from './type';
+
+export class LocalState {
+    // Common
+    currView: AView = 'LIST';
+    currSearchText: string = '';
+
+    // List View
+    isAllRowsSelected: boolean = false;
+
+    // Edit View
+    currListItemIdx: number = 0;
+    currNestedListItemIdx: number = 0;
+
+    // TODO: current host/path input for add/edit
+    // currHost;
+    // currPath;
+    // currLib;
+
+    // Modal
+    currModalId: string = null;
+    isModalConfirmDisabled: boolean = false;
+}
