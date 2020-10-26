9c396e8dd84c21b800754566450714d9bcbee1bb
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-27 10:58:26 +1100

    CMP - Option List View: Added typings for table row component template

--- src/component/view/option-list/type.ts
+++ src/component/view/option-list/type.ts
@@ -1,5 +1,10 @@
 import { AppState } from '../../../service/model/app-state';
 import { StateHandler } from '../../../service/state-handler';
 import { IAppProps } from '../../../service/handle/state/type';
+import { IRowComponentProps } from '../../widget/data-grid/type';
 
 export type IProps = IAppProps<AppState, StateHandler>;
+
+export interface ITbRowProps extends IRowComponentProps {
+    commonProps: IProps;
+}
