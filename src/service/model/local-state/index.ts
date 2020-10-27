ec837263115e15ea9fffea4aa7211297a5842d26
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-27 20:51:29 +1100

    SERVICE - Local state model: Added additional properties and moved `isModalConfirmDisabled` (duplicate of setting model)

--- src/service/model/local-state/index.ts
+++ src/service/model/local-state/index.ts
@@ -1,17 +1,25 @@
 import { AView, AObj } from './type';
+import { HostRuleConfig } from '../rule-config';
 
 export class LocalState {
     // Common
     currView: AView = 'LIST';
-    currSearchText: string = '';
+    searchedText: string = '';
+    searchedRules: HostRuleConfig[] = null;
 
     // List View
     isAllRowsSelected: boolean = false;
 
-    // Edit View
+    // Edit View (TODO: rename it to editListItem)
     currListItem: AObj = null;
 
     // Modal
     currModalId: string = null;
-    isModalConfirmDisabled: boolean = false;
+
+    targetEditItem: HostRuleConfig = null;
+    isTargetEditItemIdValid: boolean = false;
+    isTargetEditItemValValid: boolean = false;
+
+    targetRmvItemIdx: number = null;
+    targetRmvItemParentIdx: number = null;
 }
