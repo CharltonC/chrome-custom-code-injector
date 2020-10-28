794da6665f057b1977a5e49c8d97fd1c2a93a4bb
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-28 11:15:10 +1100

    SERVICE - Modal Constant: Changed class to const

--- src/service/constant/modals/index.ts
+++ src/service/constant/modals/index.ts
@@ -1,43 +1,41 @@
-class Modals {
-    SETTING = {
+export const modals = {
+    SETTING: {
         id: 'modal-setting',
         txt: 'Default Settings'
-    };
+    },
 
-    IMPORT_SETTING = {
+    IMPORT_SETTING: {
         id: 'modal-setting-import',
         txt: 'Import Configuration from a `*.json` file'
-    };
+    },
 
-    EXPORT_SETTING = {
+    EXPORT_SETTING: {
         id: 'modal-setting-export',
         txt: 'Export Configuration to a `*.json` file'
-    };
+    },
 
-    DELETE = {
+    DELETE: {
         id: 'modal-delete-confirm',
         txt: 'Confirmation'
-    };
+    },
 
-    ADD_HOST = {
+    ADD_HOST: {
         id: 'modal-host-add',
         txt: 'Add Host'
-    };
+    },
 
-    ADD_PATH = {
+    ADD_PATH: {
         id: 'modal-path-add',
         txt: 'Add Path'
-    };
+    },
 
-    ADD_LIB = {
+    ADD_LIB: {
         id: 'modal-lib-add',
         txt: 'Add a library from a URL (e.g. CDN)'
-    };
+    },
 
-    EDIT_LIB = {
+    EDIT_LIB: {
         id: 'modal-lib-edit',
         txt: 'Edit a Library Url'
-    };
-}
-
-export const modals = new Modals();
+    },
+};
