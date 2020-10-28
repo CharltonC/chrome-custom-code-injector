9cff14b6afe8b273173cd11cafb322744d56fd1b
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-28 17:35:37 +1100

    SERVICE - Validation Rules constant: * Added regex rule for Url path with tests * Added `beforeEach` block to extract variable

--- src/service/constant/validation/index.spec.ts
+++ src/service/constant/validation/index.spec.ts
@@ -4,35 +4,61 @@ import { validationRules } from ".";
 describe('Validation Rules', () => {
     let gte3Char: IValidationRule;
     let urlHost: IValidationRule;
+    let urlPath: IValidationRule;
+    let rule;
 
     beforeEach(() => {
-        ({ gte3Char, urlHost } = validationRules);
+        ({ gte3Char, urlHost, urlPath } = validationRules);
     });
 
     describe('3 or more alphabets only', () => {
+        beforeEach(() => {
+            ({ rule } = gte3Char);
+        });
+
         it('should test truthy when value has 3 or more alphabets only', () => {
-            expect(gte3Char.rule.test('abc')).toBeTruthy();
+            expect(rule.test('abc')).toBeTruthy();
         });
 
         it('should test falsy when value has less than 3 alphabets', () => {
-            expect(gte3Char.rule.test('ab')).toBeFalsy();
+            expect(rule.test('ab')).toBeFalsy();
         });
 
         it('should test falsy when value contains space or non-alpahbet character', () => {
-            expect(gte3Char.rule.test('ab c')).toBeFalsy();
-            expect(gte3Char.rule.test('ab-c')).toBeFalsy();
+            expect(rule.test('ab c')).toBeFalsy();
+            expect(rule.test('ab-c')).toBeFalsy();
         });
     });
 
     describe('should test for host of a URL', () => {
+        beforeEach(() => {
+            ({ rule } = urlHost);
+        });
+
         it('shold test truthy when value is valid', () => {
-            expect(urlHost.rule.test('www.google.com')).toBeTruthy();
-            expect(urlHost.rule.test('google.com')).toBeTruthy();
+            expect(rule.test('www.google.com')).toBeTruthy();
+            expect(rule.test('google.com')).toBeTruthy();
         });
 
         it('shold test falsy when value is invalid', () => {
-            expect(urlHost.rule.test('google')).toBeFalsy();
-            expect(urlHost.rule.test('google.com/abc')).toBeFalsy();
+            expect(rule.test('google')).toBeFalsy();
+            expect(rule.test('google.com/abc')).toBeFalsy();
+        });
+    });
+
+    describe('should test for path of a URL', () => {
+        beforeEach(() => {
+            ({ rule } = urlPath);
+        });
+
+        it('shold test truthy when value is valid', () => {
+            expect(rule.test('/a')).toBeTruthy();
+            expect(rule.test('/abc/')).toBeTruthy();
+        });
+
+        it('shold test falsy when value is invalid', () => {
+            expect(rule.test('/')).toBeFalsy();
+            expect(rule.test('abc/')).toBeFalsy();
         });
     });
 });
--- src/service/constant/validation/index.ts
+++ src/service/constant/validation/index.ts
@@ -1,13 +1,20 @@
 import { IValidationRule } from './type';
 
-export const validationRules: Record<string, IValidationRule> = {
-    gte3Char: {
-        rule: /^[a-zA-Z]{3,}$/,
+class ValidatioNRules {
+    readonly gte3Char: IValidationRule = {
+        rule: /^[a-zA-Z0-9]{3,}$/,
         msg: 'must be 3 or more characters without whitespace'
-    },
+    };
 
-    urlHost: {
+    readonly urlHost: IValidationRule = {
         rule: /^(www\.)?(([a-z0-9]+(-|_)?)+\.)+[a-z0-9]+$/,
         msg: 'must be a domain, e.g. www.google.com'
-    }
+    };
+
+    readonly urlPath: IValidationRule = {
+        rule: /^\/([^?\/]+)/,
+        msg: 'must be Url path, e.g. /home'
+    };
 }
+
+export const validationRules = new ValidatioNRules();
