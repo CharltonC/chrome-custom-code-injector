2ebd1f1116fa996074e70adff1fde9695e3d6258
Charlton Cheng <charltoncheng86@gmail.com>
2020-10-30 12:29:25 +1100

    SERVICE - Local State Model: Added pagination state and updated tests

--- src/service/model/local-state/index.spec.ts
+++ src/service/model/local-state/index.spec.ts
@@ -3,14 +3,21 @@ import { LocalState } from '.';
 describe('Local State (Non-Persisting)', () => {
     it('should return default value', () => {
         expect(new LocalState()).toEqual({
-            currView: 'LIST',
             searchedText: '',
             searchedRules: null,
+            selectedRowKeys: {},
+            pgnPageIdx: 0,
+            pgnIncrmIdx: 0,
+            pgnItemStartIdx: 0,
+            pgnItemEndIdx: 20,
+
+            currView: 'LIST',
             isAllRowsSelected: false,
             expdRowId: null,
             currListItem: null,
             currModalId: null,
             allowModalConfirm: false,
+
             targetItem: null,
             isTargetItemIdValid: false,
             isTargetItemValValid: false,
--- src/service/model/local-state/index.ts
+++ src/service/model/local-state/index.ts
@@ -1,24 +1,40 @@
 import { AView, AObj } from './type';
+import { resultsPerPage } from '../../constant/result-per-page';
 import { HostRuleConfig } from '../rule-config';
+import { Setting } from '../../model/setting';
+
+const { resultsPerPageIdx } = new Setting();
+const totalPerPage: number = resultsPerPage[resultsPerPageIdx];
 
 export class LocalState {
-    // Common
-    currView: AView = 'LIST';
+    //// Common
+    // - Search
     searchedText: string = '';
     searchedRules: HostRuleConfig[] = null;
 
-    // List View
+    // - Data Grid Rows
+    selectedRowKeys: Record<string, boolean> = {};
+    pgnPageIdx: number = 0;
+    pgnIncrmIdx: number = resultsPerPageIdx;
+    pgnItemStartIdx: number = 0;
+    pgnItemEndIdx: number = totalPerPage;
+
+    //// Views
+    // Current
+    currView: AView = 'LIST';
+
+    // - List View
     isAllRowsSelected: boolean = false;
     expdRowId: string = null;
 
-    // Edit View (TODO: rename it to editListItem)
+    // - Edit View (TODO: rename it to editListItem)
     currListItem: AObj = null;
 
-    // Modal
+    //// Modal
     currModalId: string = null;
     allowModalConfirm: boolean = false;
 
-    // Host/Path add/edit item (table row)
+    //// Host/Path add/edit item (table row)
     targetItem: HostRuleConfig = null;
     targetItemIdx: number = null;
     targetChildItemIdx: number = null;
