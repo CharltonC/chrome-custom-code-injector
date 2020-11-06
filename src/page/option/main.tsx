200c5c59483511aa98456cc1757fb9028411c1c2
Charlton Cheng <charltoncheng86@gmail.com>
2020-11-06 17:29:19 +1100

    PAGE - Option: Commented out logic for now to silence component props lint error

--- src/page/option/main.tsx
+++ src/page/option/main.tsx
@@ -1,13 +1,14 @@
-// Ant Design
-// - `import {Button} from 'antd';`             does NOT work for Prod build (as it import all modules resulting in large file)
-//                                              (even specifying in babel config `"libraryDirectory": "lib"` does not work)
-// - `import Button from 'antd/es/button';`     does NOT work for JEST (as it doesnt allow ES6 import/export in "node_modules/")
-// - `import Button from 'antd/lib/button';`    works for both Prod build (Browserify) + JEST
+// // Ant Design
+// // - `import {Button} from 'antd';`             does NOT work for Prod build (as it import all modules resulting in large file)
+// //                                              (even specifying in babel config `"libraryDirectory": "lib"` does not work)
+// // - `import Button from 'antd/es/button';`     does NOT work for JEST (as it doesnt allow ES6 import/export in "node_modules/")
+// // - `import Button from 'antd/lib/button';`    works for both Prod build (Browserify) + JEST
 
-import React from 'react';
-import { render } from 'react-dom';
-import { OptionApp } from '../../component/app/option';
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { OptionApp } from '../../component/app/option';
 
-const containerElem = document.createElement('div');
-document.body.appendChild(containerElem);
-render(<OptionApp />, containerElem);
+// const containerElem = document.createElement('div');
+// document.body.appendChild(containerElem);
+// TODO: wrap with state
+// render(<OptionApp />, containerElem);
