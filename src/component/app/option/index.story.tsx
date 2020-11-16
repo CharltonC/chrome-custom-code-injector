520515288a6727a7d170bcdbc0f602785fa954fb
Charlton Cheng <charltoncheng86@gmail.com>
2020-11-17 10:08:54 +1100

    CMP - OPTION APP: Moved mock state under mock folder

--- src/component/app/option/index.story.tsx
+++ src/component/app/option/index.story.tsx
@@ -1,52 +1,8 @@
 import React from 'react';
 import { StateHandler } from '../../../service/state-handler/root';
 import { StateHandle } from '../../../service/handle/state';
-import { HostRuleConfig, PathRuleConfig, LibRuleConfig } from '../../../service/model/rule-config';
-import { AppState } from '../../../service/model/app-state';
+import { createMockAppState } from '../../../mock/app-state';
 import { OptionApp } from '.';
-
-const mockHostRuleConfigs: [string, string][] = [
-    ['Amazon', 'www.amazon.com'],
-    ['Ebay', 'www.ebay.com'],
-    ['Gumtree', 'www.gumtree.com'],
-]
-
-const mockPathRuleConfigs: [string, string][] = [
-    ['home', 'home'],
-    ['deal', 'deals/today/'],
-    ['panasonic', 'electronics/panasonic'],
-]
-
-const mockLibRuleConfigs: [string, string][] = [
-    ['jQuery', 'jquery.com'],
-    ['Angular', 'angular.io'],
-    ['Bootstrap', 'bootstrap.com'],
-]
-
-const createMockAppState = (): AppState =>  {
-    const { rules, setting, localState } = new AppState();
-
-    mockHostRuleConfigs.forEach(([ hostId, hostValue]) => {
-        const hostRuleConfig = new HostRuleConfig(hostId, hostValue);
-
-        mockPathRuleConfigs.forEach(([ pathId, pathValue]) => {
-            const pathRuleConfig = new PathRuleConfig(`${hostId}-${pathId}`, pathValue);
-            hostRuleConfig.paths.push(pathRuleConfig);
-
-            mockLibRuleConfigs.forEach(([ libId, libValue ]) => {
-                pathRuleConfig.libs.push(new LibRuleConfig(`${hostId}-${pathId}-${libId}`, libValue));
-            });
-        })
-
-        mockLibRuleConfigs.forEach(([ libId, libValue ]) => {
-            hostRuleConfig.libs.push(new LibRuleConfig(`${hostId}-${libId}`, libValue));
-        });
-
-        rules.push(hostRuleConfig);
-    });
-
-    return { localState, setting, rules };
-}
 
 const App = StateHandle.init(OptionApp, {
     root: [ createMockAppState(), new StateHandler() ],
