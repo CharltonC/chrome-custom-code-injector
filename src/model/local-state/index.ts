649cff82066b80f964b45a0c54d179df8ee8ace2
Charlton Cheng <charltoncheng86@gmail.com>
2020-11-18 13:56:44 +1100

    MODEL - Local State: * Removed unnecessary unit tests * Added key for sort option * Rearranged keys order

--- src/model/local-state/index.ts
+++ src/model/local-state/index.ts
@@ -1,42 +1,50 @@
-import { AView } from './type';
 import { HostRuleConfig } from '../rule-config';
 import { Setting } from '../setting';
+import { AView } from './type';
+import * as TSort from '../../service/sort-handle/type';
 
 const { resultsPerPageIdx } = new Setting();
 
 export class LocalState {
-    //// Common
-    // - Search
+    //// HEADER
+    // * Search
     searchedText: string = '';
     searchedRules: HostRuleConfig[] = null;
 
-    // Data Grid Rows
-    // temp storage which points to the current full set of sorted data if exist or plain data (either searched or non-search)
-    // - used by Modal Delete Confirm `onDelModalConfirm`
+    // * Import/Export
+    importFile: File = null;
+    exportFileName: string = null;
+
+    // * Views
+    currView: AView = 'LIST';
+
+    //// DATA GRID ROWS
+    // * rows used by Modal Delete Confirm `onDelModalConfirm`
+    // - temp storage which points to the current full set of sorted data if exist or plain data (either searched or non-search)
     dataSrc: HostRuleConfig[] = null;
 
+    // * sort
+    sortOption: Partial<TSort.IOption> = { reset: true };
+
+    // * select
+    areAllRowsSelected: boolean = false;
     selectedRowKeys: Record<string, boolean> = {};
+
+    // * expand (only for allow 1 row to be expanded at the same time)
+    expdRowId: string = null;
+
+    // * pagination (TODO: make this an pagination state object)
     pgnPageIdx: number = 0;
     pgnIncrmIdx: number = resultsPerPageIdx;
     pgnItemStartIdx: number = 0;
     pgnItemEndIdx: number = null;
 
-    // - Import/Export
-    importFile: File = null;
-    exportFileName: string = null;
-
-    //// Views
-    currView: AView = 'LIST';
-
-    // - List View
-    areAllRowsSelected: boolean = false;
-    expdRowId: string = null;
-
-    //// Modal
+    //// MODAL
+    // * ID & Confirm disabled
     currModalId: string = null;
     allowModalConfirm: boolean = false;
 
-    //// Host/Path add/edit item (table row)
+    // * Host/Path add/edit item (table row)
     targetItem: HostRuleConfig = null;
     targetItemIdx: number = null;
     targetChildItemIdx: number = null;
