e82ea71443dc3bdf6ae9eea90ed14596c405cbda
Charlton Cheng <charltoncheng86@gmail.com>
2020-11-21 15:28:11 +1100

    CONSTANT - Validation Rule: Added rule for library url

--- src/constant/validation-rule/index.ts
+++ src/constant/validation-rule/index.ts
@@ -1,11 +1,12 @@
 import { ValidationHandle } from '../../service/validation-handle';
 
-const { gteChar, nonEmptyFile, maxFileSize, fileName, urlHost, urlPath } = new ValidationHandle();
+const { gteChar, nonEmptyFile, maxFileSize, fileName, urlHost, urlPath, url } = new ValidationHandle();
 
 export const validationRule = {
     ruleId: [ gteChar(2) ],
     ruleUrlHost: [ urlHost ],
     ruleUrlPath: [ urlPath ],
+    libUrl: [ url ],
     importConfig: [ nonEmptyFile, maxFileSize ],
     exportConfig: [ fileName ],
 };
