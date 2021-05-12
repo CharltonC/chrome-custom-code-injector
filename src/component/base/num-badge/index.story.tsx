c14d258b04e259bc1b28f5ffe7dd5237f66ea116
Charlton Cheng <charltoncheng86@gmail.com>
2021-05-12 17:52:57 +1000

    CMP - Number Badge: Fixed story error

--- src/component/base/num-badge/index.story.tsx
+++ src/component/base/num-badge/index.story.tsx
@@ -5,7 +5,7 @@ export default {
     title: 'Base/Number Badge'
 };
 
-export const NoNumber = <NumBadge />;
+export const NoNumber = () => <NumBadge />;
 export const LessThan0 = () => <NumBadge total={-1} />;
 export const LargerThan9 = () => <NumBadge total={11} />;
 export const GreaterThan0AndLessThanEqualTo9 = <NumBadge total={6} />;
