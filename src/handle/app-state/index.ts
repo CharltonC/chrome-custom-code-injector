4d9fa19ebc1724cc5df35a62358c4a50c9620e11
Charlton Cheng <charltoncheng86@gmail.com>
2021-07-21 14:29:26 +1000

    HANDLE - App State: Renamed folder for edit and list views

--- src/handle/app-state/index.ts
+++ src/handle/app-state/index.ts
@@ -1,7 +1,7 @@
 import { StateHandle } from '../state';
-import { EditViewStateHandler } from './edit-view';
+import { EditViewStateHandler } from './option-edit-view';
 import { ModalStateHandler } from './modal';
-import { ListViewStateHandler } from './list-view';
+import { ListViewStateHandler } from './option-list-view';
 import { IStateHandler } from './type';
 
 export const AppStateHandler = StateHandle.BaseStateHandler.join<IStateHandler>([
--- src/handle/app-state/type.ts
+++ src/handle/app-state/type.ts
@@ -1,5 +1,5 @@
-import { EditViewStateHandler } from './edit-view';
+import { EditViewStateHandler } from './option-edit-view';
 import { ModalStateHandler } from './modal';
-import { ListViewStateHandler } from './list-view';
+import { ListViewStateHandler } from './option-list-view';
 
 export interface IStateHandler extends ListViewStateHandler, EditViewStateHandler, ModalStateHandler {}
