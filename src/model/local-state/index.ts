cc4f5b7018997061fb38e53e474e6209ede52156
Charlton Cheng <charltoncheng86@gmail.com>
2021-07-25 14:30:41 +1000

    MODEL - Local State: Removed old data grid associated state properties

--- src/model/local-state/index.ts
+++ src/model/local-state/index.ts
@@ -32,33 +32,4 @@ export class LocalState {
     //// DATA GRID ROWS
     ruleDataGrid = new DataGridState();
     libDataGrid = new DataGridState();
-
-    // // Rows used by Modal Delete Confirm `onDelModalConfirm`
-    // // - temp storage which points to the current full set of sorted data if exist or plain data (either searched or non-search)
-    // // - unpaginated
-    // dataSrc: HostRuleConfig[] = null;
-
-    // // Sort
-    // sortOption: Partial<TSort.IOption> = { reset: true };
-
-    // // Select
-    // selectState: TRowSelect.IState = {
-    //     areAllRowsSelected: false,
-    //     selectedRowKeys: {}
-    // };
-
-    // // Expand (only for allow 1 row to be expanded at the same time)
-    // expdRowId: string = null;
-
-    // // Pagination
-    // pgnOption = {
-    //     increment: resultsPerPage,
-    //     incrementIdx: resultsPerPageIdx
-    // } as TPgn.IOption;
-
-    // pgnState = {
-    //     curr: 0,
-    //     startIdx: 0,
-    //     endIdx: null
-    // } as TPgn.IState;
 }
