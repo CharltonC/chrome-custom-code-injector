f03e5faa04493de2b06abe1e8ce439ff77341418
Charlton Cheng <charltoncheng86@gmail.com>
2021-07-27 16:54:56 +1000

    HANDLE - App State: Added type for payload

--- src/handle/app-state/sidebar/index.ts
+++ src/handle/app-state/sidebar/index.ts
@@ -4,9 +4,10 @@ import { AppState } from '../../../model/app-state';
 import { TextInputState } from '../../../model/text-input-state';
 import { DataGridState } from '../../../model/data-grid-state';
 import { LibRuleConfig } from '../../../model/rule-config';
+import * as TSideNav from '../../../component/base/side-nav/type';
 
 export class SidebarStateHandler extends StateHandle.BaseStateHandler {
-    onActiveItemChange({ rules, localState }: AppState, payload): Partial<AppState> {
+    onActiveItemChange({ rules, localState }: AppState, payload: TSideNav.IClickEvtArg): Partial<AppState> {
         const { isChild, idx, parentIdx } = payload;
         const itemIdx = isChild ? parentIdx : idx;
         const childItemIdx = isChild ? idx : null
