e74153a121fa54d67d0501d514a961be30997302
Charlton Cheng <charltoncheng86@gmail.com>
2021-07-27 20:55:13 +1000

    Revert "HANDLE - App State: Included logic when combined with search scenario"
    This reverts commit 132f1342e585e702b3871bfbbbdb1872ba4bff92.

--- src/handle/app-state/view/index.ts
+++ src/handle/app-state/view/index.ts
@@ -5,44 +5,23 @@ import { LocalState } from '../../../model/local-state';
 import { HandlerHelper } from '../helper';
 import { TextInputState } from '../../../model/text-input-state';
 import { DataGridState } from '../../../model/data-grid-state';
-import { HostRuleConfig } from '../../../model/rule-config';
 
 const { defState: rowSelectDefState } = new RowSelectHandle();
 
 export class ViewStateHandler extends StateHandle.BaseStateHandler {
     onEditView({ rules, localState }: AppState, { isHost, idx, parentCtxIdx }): Partial<AppState> {
-        let activeRule = {
+        // Set the current active item
+        const activeRule = {
             isHost,
             idx:  isHost ? idx : parentCtxIdx,
             pathIdx: isHost ? null : idx,
         };
 
-        // TODO: Check sort status (i.e. if data table is using `dataSrc`)
-
-        // TODO: this is also `dataSrc`?
-        // TODO: refactor as method?
-        // If search, we need to find the host item to be edited (relative to search rules),
-        // then find the corresp. index in `rules`
-        const { searchedRules } = localState;
-        if (searchedRules?.length) {
-            const item = HandlerHelper.getActiveItem({
-                rules: searchedRules,
-                isActiveItem: true,
-                isHost: true,
-                idx: isHost ? idx : parentCtxIdx,
-            }) as HostRuleConfig;
-
-            activeRule = {
-                ...activeRule,
-                idx: rules.indexOf(item),
-            }
-        }
-
         // Get the title and value of the item to be used in input placeholders
         const { title, value } = HandlerHelper.getActiveItem({
+            rules,
             ...activeRule,
             isActiveItem: true,
-            rules,
         });
         const activeTitleInput = new TextInputState({ value: title });
         const activeValueInput = new TextInputState({ value });
