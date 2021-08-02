bf0d52c49c0e0b5f76b56ff7ace5e9330b244115
Charlton Cheng <charltoncheng86@gmail.com>
2021-08-02 20:16:56 +1000

    STATE - Option List View Handler: * Updated logic to match recent local state API changes * Simplified search handling logic * Replaced app state type * Moved all modal associated methods to modal state handler

--- src/state/handler/option-list-view/index.ts
+++ src/state/handler/option-list-view/index.ts
@@ -1,121 +1,119 @@
 import { StateHandle } from '../../../handle/state';
-import { AppState } from '../../model';
-import { HostRuleConfig } from '../../../data/model/rule-config';
+import { IAppState } from '../../model/type';
 import { TextInputState } from '../../model/text-input-state';
 import { ActiveRuleState } from '../../model/active-rule-state';
 import { RowSelectHandle } from '../../../handle/row-select';
-import { HandlerHelper } from '../helper';
-import { DataCrudHandle } from '../../../data/handler';
+import { dataHandle } from '../../../data/handler';
 import * as TPgn from '../../../handle/pagination/type';
 import * as TSort from '../../../handle/sort/type';
-import { IStateHandler } from '../type';
 
-const { defState: rowSelectDefState } = new RowSelectHandle();
 const rowSelectHandle = new RowSelectHandle();
 
 export class OptionListViewHandler extends StateHandle.BaseStateHandler {
     //// DATA GRID
-    onSearchTextChange({ localState }: AppState, val: string): Partial<AppState> {
+    onSearchTextChange({ localState }: IAppState, payload): Partial<IAppState> {
+        const { value } = payload;
         return {
             localState: {
                 ...localState,
-                searchedText: val,
-            }
-        };
-    }
-
-    onSearchTextClear({ localState }: AppState) {
-        return {
-            localState: {
-                ...localState,
-                searchedText: ''
-            }
-        };
-    }
-
-    onSearchPerform({ localState, rules }: AppState, val: string) {
-        const { hsText } = HandlerHelper;
-        const searchedRules: HostRuleConfig[] = val
-            .split(/\s+/)
-            .reduce((filteredRules: HostRuleConfig[], text: string) => {
-                text = text.toLowerCase();
-
-                return filteredRules.filter(({ title, value, paths }: HostRuleConfig) => {
-                    // Check Row Id, Value
-                    if (hsText([title, value], text)) return true;
-
-                    // Check Sub Row Id, Value
-                    return paths.some(({ title: subTitle, value: subValue }) => hsText([subTitle, subValue], text));
-                });
-            }, rules);
-
-        return {
-            localState: {
-                ...localState,
-                searchedRules,
-            }
-        };
-    }
-
-    onPaginate({ localState}: AppState, payload: { pgnOption: TPgn.IOption, pgnState: TPgn.IState }) {
-        return {
-            localState: {
-                ...localState,
-
-                ruleDataGrid: {
-                    ...localState.ruleDataGrid,
-
-                    // clear all selections
-                    selectState: {
-                        areAllRowsSelected: false,
-                        selectedRowKeyCtx: {},
-                    },
-
-                    // Update pagination
-                    ...payload
+                listView: {
+                    ...localState.listView,
+                    searchText: value,
                 }
             }
         };
     }
 
-    onSort({ localState }: AppState, { sortOption }: { sortOption: TSort.IOption, sortState: TSort.IState }) {
+    onSearchTextClear({ localState }: IAppState) {
         return {
             localState: {
                 ...localState,
-                ruleDataGrid: {
-                    ...localState.ruleDataGrid,
-                    sortOption
+                listView: {
+                    ...localState.listView,
+                    searchText: ''
                 }
             }
         };
     }
 
-    onRowSelectToggle({ rules, localState }: AppState, id: string, totalRules: number) {
-        const { ruleDataGrid } = localState;
-        const { selectState } = ruleDataGrid;
-        const rowSelectState = rowSelectHandle.getState({
+    onPaginate({ localState}: IAppState, payload: { pgnOption: TPgn.IOption, pgnState: TPgn.IState }) {
+        return {
+            localState: {
+                ...localState,
+                listView: {
+                    ...localState.listView,
+                    dataGrid: {
+                        ...localState.listView.dataGrid,
+
+                        // clear all selections
+                        selectState: {
+                            areAllRowsSelected: false,
+                            selectedRowKeyCtx: {},
+                        },
+
+                        // Update pagination
+                        ...payload
+                    }
+                }
+            }
+        };
+    }
+
+    onSort({ localState }: IAppState, payload: { sortOption: TSort.IOption, sortState: TSort.IState }): Partial<IAppState> {
+        const { sortOption, sortState } = payload;
+        return {
+            localState: {
+                ...localState,
+                listView: {
+                    ...localState.listView,
+                    dataGrid: {
+                        ...localState.listView.dataGrid,
+                        sortOption,
+                        sortedData: sortState.data
+                    }
+                }
+            }
+        };
+    }
+
+    onRowSelectToggle({ localState }: IAppState, payload) {
+        const { dataSrc, hostId } = payload;
+        const { dataGrid } = localState.listView;
+        const {
+            pgnState,
+            selectState: currSelectState,
+        } = dataGrid;
+
+        // Rows select state based on the datagrid source and pagination (if any)
+        const { startIdx, endIdx } = pgnState;
+        const selectState = rowSelectHandle.getState({
             isAll: false,
-            currState: selectState,
+            currState: currSelectState,
             rowsCtx: {
-                rows: rules,
-                rowKey: id,
+                // This is relative to data passed to DataGrid component (could be sorted, paginationed)
+                rows: dataSrc.slice(startIdx, endIdx),
+                rowKey: hostId,
              }
         });
 
         return {
             localState: {
                 ...localState,
-                ruleDataGrid: {
-                    ...ruleDataGrid,
-                    selectState: rowSelectState
+                listView: {
+                    ...localState.listView,
+                    dataGrid: {
+                        ...dataGrid,
+                        pgnState,
+                        selectState,
+                    }
                 }
             }
         };
     }
 
-    onRowsSelectToggle({ localState }: AppState): Partial<AppState> {
-        const { ruleDataGrid } = localState;
-        const { selectState } = ruleDataGrid;
+    onRowsSelectToggle({ localState }: IAppState): Partial<IAppState> {
+        const { dataGrid } = localState.listView;
+        const { selectState } = dataGrid;
         const rowSelectState = rowSelectHandle.getState({
             isAll: true,
             currState: selectState
@@ -123,41 +121,47 @@ export class OptionListViewHandler extends StateHandle.BaseStateHandler {
         return {
             localState: {
                 ...localState,
-                ruleDataGrid: {
-                    ...ruleDataGrid,
-                    selectState: rowSelectState
+                listView: {
+                    ...localState.listView,
+                    dataGrid: {
+                        ...dataGrid,
+                        selectState: rowSelectState
+                    }
                 }
             }
         };
     }
 
-    onRowExpand({ localState }: AppState, expdState: Record<string, number>) {
-        const { ruleDataGrid } = localState;
-        const { expdRowId } = ruleDataGrid;
+    onRowExpand({ localState }: IAppState, expdState: Record<string, number>) {
+        const { dataGrid } = localState.listView;
+        const { expdRowId } = dataGrid;
         const [ id ]: string[] = Object.getOwnPropertyNames(expdState);
 
         return {
             localState: {
                 ...localState,
-                ruleDataGrid: {
-                    ...ruleDataGrid,
-                    expdRowId: id === expdRowId ? null : id
+                listView: {
+                    ...localState.listView,
+                    dataGrid: {
+                        ...dataGrid,
+                        expdRowId: id === expdRowId ? null : id
+                    }
                 }
             }
         };
     }
 
-    onEditView({ rules, localState }: AppState, payload): Partial<AppState> {
-        const { type, hostId, pathId } = payload;
+    onEditView({ rules, localState }: IAppState, payload): Partial<IAppState> {
+        const { hostId, pathId } = payload;
+        const { editView } = localState;
 
         // Find the index in rules
-        const activeRule = new ActiveRuleState({ type, hostId, pathId });
+        const ruleIdCtx = new ActiveRuleState({ hostId, pathId });
 
         // Get the title and value of the item to be used in input placeholders
-        const { hostIdx, pathIdx } = DataCrudHandle.getRuleIdxCtx({ rules, type, hostId, pathId });
-        const { title, value } = DataCrudHandle.getRuleFromIdx({ rules, type, hostIdx, pathIdx });
-        const activeTitleInput = new TextInputState({ value: title });
-        const activeValueInput = new TextInputState({ value });
+        const { title, value } = dataHandle.getRuleFromIdCtx(rules, { hostId, pathId });
+        const titleInput = new TextInputState({ value: title });
+        const valueInput = new TextInputState({ value });
 
         return {
             localState: {
@@ -165,169 +169,14 @@ export class OptionListViewHandler extends StateHandle.BaseStateHandler {
 
                 // Edit Mode & active item, item states
                 isListView: false,
-                activeRule,
-                activeTitleInput,
-                activeValueInput,
 
-                // clear the row select state ready for use for DataGrid component in Edit View]
-                ruleDataGrid: {
-                    ...localState.ruleDataGrid,
-                    selectState: rowSelectDefState,
+                editView: {
+                    ...editView,
+                    ruleIdCtx,
+                    titleInput,
+                    valueInput,
                 }
             }
-        };
-    }
-
-    //// REMOVE HOST/PATH
-    onListViewDelModal(state: AppState, payload) {
-        const { localState, setting } = state;
-        const { showDeleteModal } = setting;
-        const { reflect } = this as unknown as IStateHandler;
-        const baseLocalState = {
-            ...localState,
-            activeModalId: removeConfirm.id,
-            delRule: new DelRuleState(payload),
-        };
-
-        return showDeleteModal
-            ? { localState: baseLocalState }
-            : reflect.onListViewDelModalOk({
-                ...state,
-                localState: baseLocalState,
-            });
-    }
-
-    onListViewDelModalOk(state: AppState) {
-        const { rules, localState } = state;
-        const { ruleDataGrid, delRule } = localState;
-
-        if (isMultiple) {
-            const { areAllRowsSelected, selectedRowKeyCtx } = ruleDataGrid.selectState;
-            if (areAllRowsSelected) {
-                DataCrudHandle.rmvAllHosts({
-                    rules,
-                    dataGridRules,
-                    ruleDataGrid,
-                });
-            } else {
-                DataCrudHandle.rmvPartialHosts({
-                    rules,
-                    dataGridRules,
-                    selectedRowKeyCtx
-                });
-            }
-
-        } else {
-            if (Number.isInteger(pathIdx)) {
-                reflect.rmvListViewPath({ hostItem, pathIdx });
-            } else {
-                reflect.rmvListViewHost({
-                    rules,
-                    hostItem
-                });
-            }
-        }
-
-        const resetLocalState: LocalState = {
-            ...reflect.onModalCancel(state).localState,
-            ruleDataGrid: {
-                ...ruleDataGrid,
-                pgnState: {
-                    ...ruleDataGrid.pgnState,
-                    curr: 0,
-                    startIdx: 0,
-                    endIdx: null
-                },
-                selectState: {              // in case of side-effect on `selectedRowKeyCtx` state
-                    areAllRowsSelected: false,
-                    selectedRowKeyCtx: {},
-                }
-            }
-        };
-
-        return {
-            localState: {
-                ...localState,
-                ...resetLocalState,
-            }
-        };
-    }
-
-    // Search context here
-    onAddHostModal({ localState }: AppState) {
-        return {
-            localState: {
-                ...localState,
-                activeModalId: editHost.id
-            }
-        };
-    }
-
-    // Search context here
-    onAddHostModalOk(state: AppState) {
-        const { localState, rules, setting } = state;
-
-        const { modalTitleInput, modalValueInput } = localState;
-        const title: string = modalTitleInput.value;
-        const host: string = modalValueInput.value;
-        const hostRule = new HostRuleConfig(title, host);
-        Object.assign(hostRule, setting.defRuleConfig);
-        rules.push(hostRule);
-
-        const modalResetState = this.reflect.onModalCancel(state);
-        return {
-            ...modalResetState,
-            rules: rules.concat([])
-        };
-    }
-
-    onListViewAddPathModal({ localState }: AppState, payload) {
-        // TODO: List view OR Edit view
-        const { hostId } = payload;
-        return {
-            localState: {
-                ...localState,
-                activeModalId: modals.editPath.id,
-                // TODO: which host to add
-            }
-        };
-    }
-
-    onListViewAddPathModalOk(state: AppState) {
-        // TODO: List view OR Edit view
-        const { localState, rules, setting } = state;
-        const {
-            modalTitleInput, modalValueInput, modalRuleIdx,
-            isListView, activeRule,
-        } = localState;
-
-        // Add new path to the target rule
-        const title: string = modalTitleInput.value;
-        const path: string = modalValueInput.value;
-        const pathRule = new PathRuleConfig(title, path);
-        Object.assign(pathRule, setting.defRuleConfig);
-        const { paths } = rules[modalRuleIdx];
-        const lastPathIdx = paths.length;   // get last index where the new path will be located/added
-        paths.push(pathRule);
-
-        // If it is Edit View, Make the added path as current active item
-        const activeItemState = isListView ? {} : {
-            activeRule: new ActiveRuleState({
-                ...activeRule,
-                isHost: false,
-                pathIdx: lastPathIdx
-            }),
-            activeTitleInput: new TextInputState({ value: title }),
-            activeValueInput: new TextInputState({ value: path }),
-        };
-
-        const resetState = this.reflect.onModalCancel(state);
-        return {
-            localState: {
-                ...resetState.localState,
-                ...activeItemState
-            },
-            rules: [...rules]
         };
     }
 }
