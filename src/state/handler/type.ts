2080c4fb0e19b13c22d3e1537403a2ebc50ae6b5
Charlton Cheng <charltoncheng86@gmail.com>
2021-08-04 16:04:28 +1000

    STATE - Handler: Added types

--- src/state/handler/type.ts
+++ src/state/handler/type.ts
@@ -1,5 +1,18 @@
 import { ModalStateHandler } from './modal';
 import { OptionListViewHandler } from './option-list-view';
+import * as TPgn from '../../handle/pagination/type';
+import * as TSort from '../../handle/sort/type';
 
 // Used for casting `this.reflect` property inside individual Handler
 export interface IStateHandler extends ModalStateHandler, OptionListViewHandler {}
+
+// Payload
+export interface IOnPaginatePayload {
+    pgnOption: TPgn.IOption;
+    pgnState: TPgn.IState;
+}
+
+export interface IOnSortPayload {
+    sortOption: TSort.IOption,
+    sortState: TSort.IState;
+}
