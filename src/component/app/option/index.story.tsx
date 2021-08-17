d9323cacac39ce6d7a067d460df5c19502fad6ee
Charlton Cheng <charltoncheng86@gmail.com>
2021-08-17 19:53:24 +1000

    CMP - Option App: Added example with working rules

--- src/component/app/option/index.story.tsx
+++ src/component/app/option/index.story.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
-import { AppStateManager } from '../../../state/manager';
+import { AppStateHandle } from '../../../handle/app-state';
 import { StateHandle } from '../../../handle/state';
-import { createMockAppState } from '../../../state/mock';
+import { createMockAppState } from '../../../mock/state';
+import { mockRules } from '../../../mock/data';
 import { OptionApp } from '.';
-
-const App = StateHandle.init(OptionApp, {
-    root: [ createMockAppState(), new AppStateManager() ],
-});
 
 export default {
     title: 'App/Option',
     component: OptionApp,
 };
 
+const App = StateHandle.init(OptionApp, {
+    root: [ createMockAppState(), new AppStateHandle() ],
+});
 export const Default = () => <App />;
+
+const baseState = createMockAppState();
+const WorkingApp = StateHandle.init(OptionApp, {
+    root: [ {
+        ...baseState,
+        rules: mockRules
+    }, new AppStateHandle() ],
+});
+export const WithWorkingData = () => <WorkingApp />;
