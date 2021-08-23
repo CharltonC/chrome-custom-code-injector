12d48b6a8531d0cff7447df7478b485872da2348
Charlton Cheng <charltoncheng86@gmail.com>
2021-08-23 11:42:11 +1000

    CONSTANT - Hint Title Set: Updated text

--- src/constant/hint-title-set/index.ts
+++ src/constant/hint-title-set/index.ts
@@ -1,29 +1,29 @@
 export const hintTitleSet = {
     DOC_LINK: 'User Guide',
 
-    EXPAND_BTN: 'on: show the path rules',
+    EXPAND_BTN: 'expand to show the path rules',
 
-    HTTPS_SWITCH: 'on: host url must have HTTPS protocol',
+    HTTPS_SWITCH: 'whether the host url must have HTTPS protocol to match',
 
-    EXACT_MATCH_SWITCH: 'on: address must be an exact match',
+    EXACT_MATCH_SWITCH: 'whether an address must be an exact match (else contains address)',
 
-    CODE_EXEC_STEP_SELECT: `"dom content ready": whether the custom code should run once page is interactive; "after page load": completely loaded including images`,
+    CODE_EXEC_STEP_SELECT: `inject code either when page is interactive or page is completely loaded including images`,
 
-    JS_SWITCH: 'on: allow custom javascript code to be injected',
+    JS_SWITCH: 'whether to allow custom javascript code to be injected',
 
-    CSS_SWITCH: 'on: allow custom css code to be injected',
+    CSS_SWITCH: 'whether to allow custom css code to be injected',
 
-    LIB_SWITCH: 'on: allow custom libraries to be injected',
+    LIB_SWITCH: 'whether to allow all 3rd party libraries to be injected',
 
     ADD_HOST_BTN: 'add a host rule',
 
     ADD_PATH_BTN: 'add a path rule',
 
-    ADD_LIB_BTN: 'add a library to be injected',
+    ADD_LIB_BTN: 'add a 3rd party library to be injected',
 
-    LIB_TYPE_SELECT: 'js: javascript; css: css stylesheet',
+    LIB_TYPE_SELECT: 'library type (javascript; or css stylesheet)',
 
-    LIB_ASYNC_SWITCH: 'on: inject the js/css library in a non-blocking manner',
+    LIB_ASYNC_SWITCH: 'whether to inject the 3rd party library in a async/non-blocking manner or blocking manner',
 
-    LIB_ACTIVE_SWITCH: 'on: allow library to be injected (also requires library switch to be on)',
+    LIB_ACTIVE_SWITCH: 'whether to allow the 3rd library to be injected (also requires library switch to be on)',
 };
