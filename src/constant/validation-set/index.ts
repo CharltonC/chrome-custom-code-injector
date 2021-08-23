26112e58e2e43d4d9da2119cb6529bccff43613a
Charlton Cheng <charltoncheng86@gmail.com>
2021-08-23 21:38:17 +1000

    CONSTANT - Validation Set: Included json schema validation for import config

--- src/constant/validation-set/index.ts
+++ src/constant/validation-set/index.ts
@@ -1,12 +1,12 @@
 import { ValidationHandle } from '../../handle/validation';
 
-const { gteChar, nonEmptyFile, maxFileSize, fileName, urlHost, urlPath, url } = new ValidationHandle();
+const { gteChar, nonEmptyFile, maxFileSize, fileName, jsonFileSchema, urlHost, urlPath, url } = new ValidationHandle();
 
 export const validationSet = {
     ruleId: [ gteChar(2) ],
     ruleUrlHost: [ urlHost ],
     ruleUrlPath: [ urlPath ],
     libUrl: [ url ],
-    importConfig: [ nonEmptyFile, maxFileSize ],
+    importConfig: [ nonEmptyFile, maxFileSize, jsonFileSchema ],
     exportConfig: [ fileName ],
 };
