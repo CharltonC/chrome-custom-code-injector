c517af798f7c65d2393d8275dee7e5a98761b829
Charlton Cheng <charltoncheng86@gmail.com>
2021-08-23 11:24:08 +1000

    CONSTANT - Hint Title Set: Added constant for used in Form element `title` attribute

---
+++ src/constant/hint-title-set/index.ts
@@ -0,0 +1,29 @@
+export const HintTitleSet = {
+    DOC_LINK: 'User Guide',
+
+    EXPAND_BTN: 'on: show the path rules',
+
+    HTTPS_SWITCH: 'on: host url must have HTTPS protocol',
+
+    EXACT_MATCH_SWITCH: 'on: address must be an exact match',
+
+    CODE_EXEC_STEP_SELECT: `"dom content ready": whether the custom code should run once page is interactive; "after page load": completely loaded including images`,
+
+    JS_SWITCH: 'on: allow custom javascript code to be injected',
+
+    CSS_SWITCH: 'on: allow custom css code to be injected',
+
+    LIB_SWITCH: 'on: allow custom libraries to be injected',
+
+    ADD_HOST_BTN: 'add a host rule',
+
+    ADD_PATH_BTN: 'add a path rule',
+
+    ADD_LIB_BTN: 'add a library to be injected',
+
+    LIB_TYPE_SELECT: 'js: javascript; css: css stylesheet',
+
+    LIB_ASYNC_SWITCH: 'on: inject the js/css library in a non-blocking manner',
+
+    LIB_ACTIVE_SWITCH: 'on: allow library to be injected (also requires library switch to be on)',
+};
