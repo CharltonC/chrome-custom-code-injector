b3eb3c77bb3d5b70b303aff64b30fb3b382b297b
Charlton Cheng <charltoncheng86@gmail.com>
2021-08-25 12:40:34 +1000

    MOCK - Data: Removed mock of working data and updated relevant files

--- src/component/app/option/index.story.tsx
+++ src/component/app/option/index.story.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { AppStateHandle } from '../../../handle/app-state';
 import { StateHandle } from '../../../handle/state';
 import { createMockAppState } from '../../../mock/state';
-import { mockRules } from '../../../mock/data';
 import { OptionApp } from '.';
 
 export default {
@@ -14,12 +13,3 @@ const App = StateHandle.init(OptionApp, {
     root: [ createMockAppState(), new AppStateHandle() ],
 });
 export const Default = () => <App />;
-
-const baseState = createMockAppState();
-const WorkingApp = StateHandle.init(OptionApp, {
-    root: [ {
-        ...baseState,
-        rules: mockRules
-    }, new AppStateHandle() ],
-});
-export const WithWorkingData = () => <WorkingApp />;
--- src/mock/data/index.ts
+++ src/mock/data/index.ts
@@ -40,33 +40,3 @@ export const createMockRules = () => {
         return host;
     });
 };
-
-
-// Mock with actual working data
-const path = new PathRule('6park-path', '/au.shtml');
-const pathLib = new LibRule('6park-path-lib', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css');
-pathLib.type = 'css';
-pathLib.isOn = true;
-Object.assign(path, {
-    jsCode: 'console.log("6park-path");',
-    cssCode: '* { border: 1px solid blue !important; }',
-    isCssOn: true,
-    isJsOn: true,
-    isLibOn: true,
-    libs: [ pathLib ]
-});
-
-const host = new HostRule('6park-host', '6park.com');
-const hostLib =  new LibRule('6park-host-lib', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js');
-hostLib.isOn = true;
-Object.assign(host, {
-    jsCode: 'console.log("6park-host");',
-    cssCode: 'body { background-color: red !important; }',
-    isCssOn: true,
-    isJsOn: true,
-    isLibOn: true,
-    paths: [ path ],
-    libs: [hostLib]
-});
-
-export const mockRules = [host];
