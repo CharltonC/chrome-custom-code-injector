e075af31ff0c5452f39d4b2bb71cd8344b95aeca
Charlton Cheng <charltoncheng86@gmail.com>
2021-08-26 16:40:03 +1000

    PAGE - Popup: Added app container with id and included init logic

---
+++ src/page/popup/main.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppStateHandle } from '../../handle/app-state';
+import { StateHandle } from '../../handle/state';
+import { chromeHandle } from '../../handle/chrome';
+import { LocalState } from '../../model/local-state';
+import { SettingState } from '../../model/setting-state';
+import { PopupApp } from '../../component/app/popup';
+
+(async () => {
+    const url = await chromeHandle.getTabUrl();
+    let { rules, setting } = await chromeHandle.getState();
+
+    const appState = {
+        localState: new LocalState(rules.length),
+        setting: setting || new SettingState(),
+        rules,
+        url
+    };
+    const appStateHandle = new AppStateHandle();
+    const App = StateHandle.init(PopupApp, {
+        root: [ appState, appStateHandle ],
+    });
+
+    const $appWrapper = document.querySelector('#app');
+    render(<App />, $appWrapper);
+})();
