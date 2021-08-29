c4312fc1d0c20ec4a025b9405447e8901ed0b3d8
Charlton Cheng <charltoncheng86@gmail.com>
2021-08-29 16:31:42 +1000

    PAGE - Background: Added init logic

---
+++ src/page/background/main.ts
@@ -0,0 +1,35 @@
+import { chromeHandle } from '../../handle/chrome';
+import { dataHandle } from '../../handle/data';
+
+chrome.webRequest.onHeadersReceived.addListener(
+    evt => {
+        const { responseHeaders, url } = evt;
+
+        // check if main document, not iframe or other type of requests
+        const isMainDoc = chromeHandle.isMainframeRequest(evt);
+        if (!isMainDoc) return;
+
+        // check if match url in rules
+        // - web request callback doesn't support async, hence `then` is used
+        const pageUrl = new URL(url);
+        chromeHandle.getState().then(({ rules }) => {
+            const host = rules?.length && dataHandle.getHostFromUrl(rules, pageUrl);
+            if (!host) return;
+
+            // Alter specific CSP policies if needed
+            let csp = chromeHandle.getCsp(responseHeaders);
+            csp.value = chromeHandle.getAlteredCsp(csp.value, [
+                'script-src',
+                'style-src',
+                'style-src-elem'
+            ]);
+        });
+    },
+    {
+        urls: ['<all_urls>']
+    },
+    [
+        'blocking',
+        'responseHeaders'
+    ]
+);
