739ab048517e3f3c1b41be79f48228c685741cd5
Charlton Cheng <charltoncheng86@gmail.com>
2021-08-30 17:39:24 +1000

    PAGE - Background: Added storage/state init logic

--- src/page/background/main.ts
+++ src/page/background/main.ts
@@ -1,36 +1,40 @@
 import { chromeHandle } from '../../handle/chrome';
 import { dataHandle } from '../../handle/data';
 
-chrome.webRequest.onHeadersReceived.addListener(
-    evt => {
-        const { responseHeaders, url } = evt;
+(async () => {
+    await chromeHandle.initState();
 
-        // check if main document, not iframe or other type of requests
-        const isMainDoc = chromeHandle.isMainframeRequest(evt);
-        if (!isMainDoc) return;
+    chrome.webRequest.onHeadersReceived.addListener(
+        evt => {
+            const { responseHeaders, url } = evt;
 
-        // check if match url in rules
-        // - webrequest callback doesn't support async, hence `then` is used
-        const pageUrl = new URL(url);
-        chromeHandle.getState().then(({ rules }) => {
-            const host = rules?.length && dataHandle.getHostFromUrl(rules, pageUrl);
-            if (!host) return;
+            // check if main document, not iframe or other type of requests
+            const isMainDoc = chromeHandle.isMainframeRequest(evt);
+            if (!isMainDoc) return;
 
-            // Alter specific CSP policies if needed
-            const csp = chromeHandle.getCsp(responseHeaders);
-            if (!csp) return;
-            csp.value = chromeHandle.getAlteredCsp(csp.value, [
-                'script-src',
-                'style-src',
-                'style-src-elem'
-            ]);
-        });
-    },
-    {
-        urls: ['<all_urls>']
-    },
-    [
-        'blocking',
-        'responseHeaders'
-    ]
-);
+            // check if match url in rules
+            // - webrequest callback doesn't support async, hence `then` is used
+            const pageUrl = new URL(url);
+            chromeHandle.getState().then(({ rules }) => {
+                const host = rules?.length && dataHandle.getHostFromUrl(rules, pageUrl);
+                if (!host) return;
+
+                // Alter specific CSP policies if needed
+                const csp = chromeHandle.getCsp(responseHeaders);
+                if (!csp) return;
+                csp.value = chromeHandle.getAlteredCsp(csp.value, [
+                    'script-src',
+                    'style-src',
+                    'style-src-elem'
+                ]);
+            });
+        },
+        {
+            urls: ['<all_urls>']
+        },
+        [
+            'blocking',
+            'responseHeaders'
+        ]
+    );
+})();
