43cad7fe94d4c7eb8d6c02a05b107b2f20ce5d6b
Charlton Cheng <charltoncheng86@gmail.com>
2021-09-01 20:28:01 +1000

    HANDLE - Popup State: Added handle with tests

--- src/handle/app-state/popup-view/index.spec.ts
+++ src/handle/app-state/popup-view/index.spec.ts
@@ -1,34 +1,99 @@
-import { ChromeHandle } from '../../chrome';
-import { PopupViewStateHandle } from '.';
 import { TestUtil } from '../../../asset/ts/test-util';
+import { ChromeHandle } from '../../chrome';
+import { QueryParamHandle } from '../../query-param';
+import { DataHandle } from '../../data';
 import { AMethodSpy } from '../../../asset/ts/test-util/type';
+import { PopupViewStateHandle } from '.';
 
 describe('Popup View State Handle', () => {
     const handle = new PopupViewStateHandle();
     const mockState: any = {};
+    const mockParam = '';
     const mockFn = () => {};
     let chromeHandleSpy: AMethodSpy<ChromeHandle>;
+    let queryParamHandleSpy: AMethodSpy<QueryParamHandle>;
+    let dataHandleSpy: AMethodSpy<DataHandle>;
 
     beforeEach(() => {
         chromeHandleSpy = TestUtil.spyProtoMethods(ChromeHandle);
-        chromeHandleSpy.openExtOptionTab.mockImplementation(mockFn);
-        chromeHandleSpy.openUserguideTab.mockImplementation(mockFn);
+        queryParamHandleSpy = TestUtil.spyProtoMethods(QueryParamHandle);
+        dataHandleSpy = TestUtil.spyProtoMethods(DataHandle);
+
+        dataHandleSpy.rmvPath.mockImplementation(mockFn);
+        dataHandleSpy.rmvHost.mockImplementation(mockFn);
+        chromeHandleSpy.openExtOption.mockImplementation(mockFn);
+        chromeHandleSpy.saveState.mockImplementation(mockFn);
+        queryParamHandleSpy.createEditParam.mockReturnValue(mockParam);
+        queryParamHandleSpy.createAddHostParam.mockReturnValue(mockParam);
+        queryParamHandleSpy.createAddPathParam.mockReturnValue(mockParam);
+    });
+
+    afterEach(() => {
+        jest.clearAllMocks();
+        jest.restoreAllMocks();
     });
 
     it('Method - onOpenExtOption: should open a new option tab', () => {
-        const stateOne = handle.onOpenExtOption(mockState);
-        expect(stateOne).toEqual({});
-        expect(chromeHandleSpy.openExtOptionTab).toHaveBeenCalledWith(undefined);
+        const state = handle.onOpenExtOption();
 
-        const mockPaylod = { hostId: 'id' };
-        const stateTwo = handle.onOpenExtOption(mockState, mockPaylod);
-        expect(stateTwo).toEqual({});
-        expect(chromeHandleSpy.openExtOptionTab).toHaveBeenCalledWith(mockPaylod);
+        expect(state).toEqual({});
+        expect(chromeHandleSpy.openExtOption).toHaveBeenCalled();
+    });
+
+    it('Method - onOpenExtOptionForEdit: should open a new option tab in edit view', () => {
+        const mockPayload: any = {};
+        const state = handle.onOpenExtOptionForEdit(mockState, mockPayload);
+
+        expect(state).toEqual({});
+        expect(queryParamHandleSpy.createEditParam).toHaveBeenCalledWith(mockPayload);
+        expect(chromeHandleSpy.openExtOption).toHaveBeenCalledWith(mockParam);
+    });
+
+    it('Method - onOpenExtOptionForAddHost: should open a new option tab in list view with Add Host modal', () => {
+        const mockPayload = { hostUrl: 'host' };
+        const state = handle.onOpenExtOptionForAddHost(mockState, mockPayload);
+
+        expect(state).toEqual({});
+        expect(queryParamHandleSpy.createAddHostParam).toHaveBeenCalledWith(mockPayload.hostUrl);
+        expect(chromeHandleSpy.openExtOption).toHaveBeenCalledWith(mockParam);
+    });
+
+    it('Method - onOpenExtOptionForAddPath: should open a new option tab in list view with Add Path modal', () => {
+        const mockHostId = 'host';
+        const mockPath = 'path';
+        const mockPayload = { hostId: mockHostId, path: mockPath };
+        const state = handle.onOpenExtOptionForAddPath(mockState, mockPayload);
+
+        expect(state).toEqual({});
+        expect(queryParamHandleSpy.createAddPathParam).toHaveBeenCalledWith(mockHostId, mockPath);
+        expect(chromeHandleSpy.openExtOption).toHaveBeenCalledWith(mockParam);
     });
 
     it('Method - onOpenExtUserguide: should open a new tab for the extension user guide', () => {
+        chromeHandleSpy.openUserguide.mockImplementation(mockFn);
         const state = handle.onOpenExtUserguide();
+
         expect(state).toEqual({});
-        expect(chromeHandleSpy.openUserguideTab).toHaveBeenCalled();
+        expect(chromeHandleSpy.openUserguide).toHaveBeenCalled();
+    });
+
+    it('Method - onDelHostOrPath: should delete host or path rule and save to Chrome', () => {
+        const mockAppState: any = { rules: [] };
+        const mockPayload = { hostId: 'host' };
+
+        expect(
+            handle.onDelHostOrPath(mockAppState, mockPayload)
+        ).toEqual({ rules: mockAppState.rules });
+        expect(dataHandleSpy.rmvHost).toHaveBeenCalled();
+
+        expect(
+            handle.onDelHostOrPath(mockAppState, {
+                ...mockPayload,
+                pathId: 'path'
+            })
+        ).toEqual({ rules: mockAppState.rules });
+        expect(dataHandleSpy.rmvPath).toHaveBeenCalled();
+
+        expect(chromeHandleSpy.saveState).toHaveBeenCalledTimes(2);
     });
 });
--- src/handle/app-state/popup-view/index.ts
+++ src/handle/app-state/popup-view/index.ts
@@ -1,16 +1,49 @@
 import { StateHandle } from '../../state';
 import { chromeHandle } from '../../chrome';
+import { queryParamHandle } from '../../query-param';
 import { RuleIdCtxState } from '../../../model/rule-id-ctx-state';
-import { AppState } from '../../../model/app-state';
+import { IAppState } from '../../../component/app/popup/type';
+import { dataHandle } from '../../data';
 
 export class PopupViewStateHandle extends StateHandle.BaseStateManager {
-    onOpenExtOption(state: AppState, payload?: RuleIdCtxState) {
-        chromeHandle.openExtOptionTab(payload);
+    onOpenExtOption(): Partial<IAppState> {
+        chromeHandle.openExtOption();
         return {};
     }
 
-    onOpenExtUserguide() {
-        chromeHandle.openUserguideTab();
+    onOpenExtOptionForEdit(state: IAppState, payload: RuleIdCtxState): Partial<IAppState> {
+        const param = queryParamHandle.createEditParam(payload);
+        chromeHandle.openExtOption(param);
         return {};
+    }
+
+    onOpenExtOptionForAddHost(state: IAppState, payload: { hostUrl: string }): Partial<IAppState> {
+        const { hostUrl } = payload;
+        const param = queryParamHandle.createAddHostParam(hostUrl);
+        chromeHandle.openExtOption(param);
+        return {};
+    }
+
+    onOpenExtOptionForAddPath(state: IAppState, payload: {hostId: string, path: string}): Partial<IAppState> {
+        const { hostId, path } = payload;
+        const param = queryParamHandle.createAddPathParam(hostId, path);
+        chromeHandle.openExtOption(param);
+        return {};
+    }
+
+    onOpenExtUserguide(): Partial<IAppState> {
+        chromeHandle.openUserguide();
+        return {};
+    }
+
+    onDelHostOrPath({ rules }: IAppState, payload: RuleIdCtxState): Partial<IAppState> {
+        const { pathId } = payload;
+        pathId
+            ? dataHandle.rmvPath(rules, payload)
+            : dataHandle.rmvHost(rules, payload);
+        chromeHandle.saveState({ rules });
+        return {
+            rules: [...rules]
+        };
     }
 }
