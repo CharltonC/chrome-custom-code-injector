017cf99d856ac424a5d46e28435162394d58ffad
Charlton Cheng <charltoncheng86@gmail.com>
2021-09-06 19:39:04 +1000

    BUILD - CI: Added github token for coverage collect

--- gulp/ts-test/task.js
+++ gulp/ts-test/task.js
@@ -2,8 +2,6 @@ const { gulp, $path, $, util } = require('../common');
 const { defOption } = require('./config');
 const { collectCov } = require('yargs').argv;
 
-console.log(collectCov);
-
 module.exports = (done) => {
     if (collectCov) {
         Object.assign(defOption, {
